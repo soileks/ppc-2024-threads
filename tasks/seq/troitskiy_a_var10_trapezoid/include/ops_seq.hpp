6c6231b7ec2501143a3980f864f70bd2cdbf8f3c
TAS <tas@MacBook-Air-TAS.local>
2024-04-05 23:43:07 +0300

    first lab1 new

--- tasks/seq/troitskiy_a_var10_trapezoid/include/ops_seq.hpp
+++ tasks/seq/troitskiy_a_var10_trapezoid/include/ops_seq.hpp
@@ -8,20 +8,20 @@
 #include "core/task/include/task.hpp"
 
 class Sequentialtrapezoid : public ppc::core::Task {
-public:
-    explicit Sequentialtrapezoid(std::shared_ptr<ppc::core::TaskData> taskData_,
-                                 std::function<double(double, double)> fun_)
-    : Task(std::move(taskData_)), fun(std::move(fun_)) {}
-    bool pre_processing() override;
-    bool validation() override;
-    bool run() override;
-    bool post_processing() override;
-    
-private:
-    double a1{}, b1{}, a2{}, b2{};
-    int n1{}, n2{};
-    double res = 0.0;
-    std::function<double(double, double)> fun;
+ public:
+  explicit Sequentialtrapezoid(std::shared_ptr<ppc::core::TaskData> taskData_,
+                               std::function<double(double, double)> fun_)
+      : Task(std::move(taskData_)), fun(std::move(fun_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  double a1{}, b1{}, a2{}, b2{};
+  int n1{}, n2{};
+  double res = 0.0;
+  std::function<double(double, double)> fun;
 };
 
 double trapezoidal_integral(double a1, double b1, double a2, double b2, int n1, int n2,
--- tasks/seq/troitskiy_a_var10_trapezoid/src/ops_seq.cpp
+++ tasks/seq/troitskiy_a_var10_trapezoid/src/ops_seq.cpp
@@ -10,57 +10,57 @@ using namespace std::chrono_literals;
 
 double trapezoidal_integral(double a1, double b1, double a2, double b2, int n1, int n2,
                             const std::function<double(double, double)> &f) {
-    double h1 = (b1 - a1) / n1;
-    double h2 = (b2 - a2) / n2;
-    double integral = 0.0;
-    
-    for (int i = 0; i < n1; ++i) {
-        for (int j = 0; j < n2; ++j) {
-            double x0 = a1 + i * h1;
-            double x1 = a1 + (i + 1) * h1;
-            double y0 = a2 + j * h2;
-            double y1 = a2 + (j + 1) * h2;
-            
-            double f00 = f(x0, y0);
-            double f01 = f(x0, y1);
-            double f10 = f(x1, y0);
-            double f11 = f(x1, y1);
-            
-            integral += ((f00 + f01 + f10 + f11) / 4.0) * (x1 - x0) * (y1 - y0);
-        }
+  double h1 = (b1 - a1) / n1;
+  double h2 = (b2 - a2) / n2;
+  double integral = 0.0;
+
+  for (int i = 0; i < n1; ++i) {
+    for (int j = 0; j < n2; ++j) {
+      double x0 = a1 + i * h1;
+      double x1 = a1 + (i + 1) * h1;
+      double y0 = a2 + j * h2;
+      double y1 = a2 + (j + 1) * h2;
+
+      double f00 = f(x0, y0);
+      double f01 = f(x0, y1);
+      double f10 = f(x1, y0);
+      double f11 = f(x1, y1);
+
+      integral += ((f00 + f01 + f10 + f11) / 4.0) * (x1 - x0) * (y1 - y0);
     }
-    
-    return integral;
+  }
+
+  return integral;
 }
 
 bool Sequentialtrapezoid::pre_processing() {
-    internal_order_test();
-    // Init value for input and output
-    a1 = reinterpret_cast<double *>(taskData->inputs[0])[0];
-    b1 = reinterpret_cast<double *>(taskData->inputs[0])[1];
-    a2 = reinterpret_cast<double *>(taskData->inputs[0])[2];
-    b2 = reinterpret_cast<double *>(taskData->inputs[0])[3];
-    n1 = reinterpret_cast<int *>(taskData->inputs[1])[0];
-    n2 = reinterpret_cast<int *>(taskData->inputs[1])[1];
-    
-    return true;
+  internal_order_test();
+  // Init value for input and output
+  a1 = reinterpret_cast<double *>(taskData->inputs[0])[0];
+  b1 = reinterpret_cast<double *>(taskData->inputs[0])[1];
+  a2 = reinterpret_cast<double *>(taskData->inputs[0])[2];
+  b2 = reinterpret_cast<double *>(taskData->inputs[0])[3];
+  n1 = reinterpret_cast<int *>(taskData->inputs[1])[0];
+  n2 = reinterpret_cast<int *>(taskData->inputs[1])[1];
+
+  return true;
 }
 
 bool Sequentialtrapezoid::validation() {
-    internal_order_test();
-    // Check count elements of output
-    return taskData->inputs_count[0] == 4 && taskData->inputs_count[1] == 2 && taskData->outputs_count[0] == 1;
+  internal_order_test();
+  // Check count elements of output
+  return taskData->inputs_count[0] == 4 && taskData->inputs_count[1] == 2 && taskData->outputs_count[0] == 1;
 }
 
 bool Sequentialtrapezoid::run() {
-    internal_order_test();
-    res = trapezoidal_integral(a1, b1, a2, b2, n1, n2, fun);
-    std::this_thread::sleep_for(30ms);
-    return true;
+  internal_order_test();
+  res = trapezoidal_integral(a1, b1, a2, b2, n1, n2, fun);
+  std::this_thread::sleep_for(30ms);
+  return true;
 }
 
 bool Sequentialtrapezoid::post_processing() {
-    internal_order_test();
-    reinterpret_cast<double *>(taskData->outputs[0])[0] = res;
-    return true;
+  internal_order_test();
+  reinterpret_cast<double *>(taskData->outputs[0])[0] = res;
+  return true;
 }
