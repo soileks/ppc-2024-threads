a1b5c2d7d04534bcdb05dd7b5e748f7440b5b85d
MuUJa <mihail.podyachih@gmail.com>
2024-04-09 00:30:50 +0300

    Sequential hoare sort

---
+++ tasks/seq/podyachikh_m_hoare_sort_simple_merge/include/hoare_sort.hpp
@@ -0,0 +1,45 @@
+// Copyright 2024 Podyachikh Mikhail
+#pragma once
+
+#include <functional>
+#include <string>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+#include "random"
+
+class HoareSort : public ppc::core::Task {
+ public:
+  using obj_ty = int64_t;
+  using compare_t = std::function<bool(const obj_ty &, const obj_ty &)>;
+  using vec_t = std::vector<obj_ty>;
+
+  explicit HoareSort(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  vec_t *_data;
+  compare_t _comp;
+  /* TODO: Allow to use std::less_equal and same functions, which returns true if items are equal.
+   * Now it creates infinity recursion. */
+
+  obj_ty *get_random_pivot(obj_ty *left, obj_ty *right);
+  std::pair<HoareSort::obj_ty *, HoareSort::obj_ty *> partition(obj_ty *left, obj_ty *right);
+  void seq_hoare_sort(obj_ty *left, obj_ty *right);
+};
+
+template <typename T = HoareSort::obj_ty>
+std::vector<T> generate_random_vector(size_t size, T min_value = -1e9, T max_value = 1e9) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<T> dis(min_value, max_value);
+
+  std::vector<T> result(size);
+  for (size_t i = 0; i < size; ++i) {
+    result[i] = dis(gen);
+  }
+  return result;
+}
---
+++ tasks/seq/podyachikh_m_hoare_sort_simple_merge/src/hoare_sort.cpp
@@ -0,0 +1,83 @@
+// Copyright 2024 Podyachikh Mikhail
+
+#include "seq/podyachikh_m_hoare_sort_simple_merge/include/hoare_sort.hpp"
+
+#include <cassert>
+
+using namespace std::chrono_literals;
+
+bool HoareSort::pre_processing() {
+  internal_order_test();
+  try {
+    // Init value for input and output
+    _data = reinterpret_cast<vec_t*>(taskData->inputs[0]);
+    if (taskData->inputs.size() == 2) {
+      _comp = reinterpret_cast<compare_t*>(taskData->inputs[1])[0];
+    } else {
+      _comp = std::less<>();
+    }
+  } catch (...) {
+    std::cout << "Failed pre_processing\n" << std::endl;
+    return false;
+  }
+  return true;
+}
+
+bool HoareSort::validation() {
+  internal_order_test();
+  // Check count elements of output
+  if (taskData->inputs.empty()) return false;
+  if (taskData->inputs.size() > 2) return false;
+  if (taskData->outputs.size() > 1) return false;
+  if (taskData->outputs.empty()) {
+    return true;
+  }
+  return taskData->outputs[0] == nullptr;
+}
+
+bool HoareSort::run() {
+  internal_order_test();
+  try {
+    obj_ty* begin = _data->data();
+    obj_ty* end = _data->data() + _data->size() - 1;
+    seq_hoare_sort(begin, end);
+  } catch (...) {
+    std::cout << "Run failure\n" << std::endl;
+    return false;
+  }
+  return true;
+}
+
+bool HoareSort::post_processing() {
+  internal_order_test();
+  return true;
+}
+
+HoareSort::obj_ty* HoareSort::get_random_pivot(obj_ty* left, obj_ty* right) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<size_t> dis(0, right - left);
+  return left + dis(gen);
+}
+
+std::pair<HoareSort::obj_ty*, HoareSort::obj_ty*> HoareSort::partition(obj_ty* left, obj_ty* right) {
+  obj_ty pivot = *get_random_pivot(left, right);
+  obj_ty* i = left;
+  obj_ty* j = right;
+  while (true) {
+    while (_comp(*i, pivot)) i++;
+    while (_comp(pivot, *j)) j--;
+
+    if (i > j) {
+      return {j, i};
+    }
+    std::swap(*(i++), *(j--));
+  }
+}
+
+void HoareSort::seq_hoare_sort(obj_ty* left, obj_ty* right) {
+  if (left >= right) return;
+  auto [i, j] = partition(left, right);
+  seq_hoare_sort(left, i);
+  seq_hoare_sort(j, right);
+}
