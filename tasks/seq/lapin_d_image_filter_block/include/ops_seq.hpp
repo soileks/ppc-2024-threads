f4af41c92be9f40be5f673398e6d0a3bc1efbe89
dlapin2014 <dmitriylapin2001@gmail.com>
2024-04-11 12:56:04 +0300

    lala

---
+++ tasks/seq/lapin_d_image_filter_block/include/ops_seq.hpp
@@ -0,0 +1,20 @@
+// Copyright 2024 Lapin Dmitriy
+#pragma once
+
+#include "core/task/include/task.hpp"
+#include <vector>
+
+class BlockFilterSeq : public ppc::core::Task {
+ public:
+  explicit BlockFilterSeq(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  int width;
+  int height;
+  std::vector<std::vector<int>>* mas_in;
+  std::vector<std::vector<int>>* mas_out;
+};
