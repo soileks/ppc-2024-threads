4958fb536f3a4ba849a7f348da2fd7b37c8ee0b8
Instmois <nikita-moiseev-2003@ya.ru>
2024-04-14 23:03:32 +0300

    3

--- tasks/tbb/moiseev_n_jarvis_tbb/src/ops_tbb.cpp
+++ tasks/tbb/moiseev_n_jarvis_tbb/src/ops_tbb.cpp
@@ -68,7 +68,7 @@ std::vector<Point> tbbJarvis_Moiseev(const std::vector<Point>& points) {
     }
     currentPoint = tbb::parallel_reduce(
         tbb::blocked_range<size_t>(0, numberOfPoints), nextPoint,
-        [&currentPoint, &points](const tbb::blocked_range<size_t>& r,Point localNext) -> Point {
+        [&currentPoint, &points](const tbb::blocked_range<size_t>& r, Point localNext) -> Point {
           auto begin = r.begin(), end = r.end();
           for (auto i = begin; i != end; i++) {
             int direction = (points[i].y - currentPoint.y) * (localNext.x - currentPoint.x) -
