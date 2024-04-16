c95288249875d01b302beccabe9269809a1db2fa
Anton <anton.s.savchuk@gmail.com>
2024-04-16 15:03:59 +0300

    Inital commit

--- tasks/tbb/savchuk_a_crs_matmult_tbb/func_tests/main.cpp
+++ tasks/tbb/savchuk_a_crs_matmult_tbb/func_tests/main.cpp
@@ -17,7 +17,7 @@ TEST(savchuk_a_crs_matmult_tbb, test_sizes) {
   std::vector<Complex> in2(n2 * m2);
 
   std::vector<Complex> out(n1 * m2);
-  
+
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
   taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in1.data()));
@@ -134,7 +134,6 @@ TEST(savchuk_a_crs_matmult_tbb, test_multy_correct) {
   taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
   taskDataSeq->outputs_count.emplace_back(n1);
   taskDataSeq->outputs_count.emplace_back(m2);
-
 
   // Create Task
   SavchukCRSMatMultTBBSequential savchukCRSMatMultTBBSequential(taskDataSeq);
