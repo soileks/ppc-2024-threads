719c1512675b00ded26d9c09812b9b62b5ae6df0
Veselov Mikhail <veselovmikhailwork@gmail.com>
2024-04-22 11:35:51 +0300

    restart

--- tasks/omp/veselov_m_matrcomplexmultyCCS_omp/src/ops_omp.cpp
+++ tasks/omp/veselov_m_matrcomplexmultyCCS_omp/src/ops_omp.cpp
@@ -14,7 +14,6 @@ bool SparseMatrixComplexMultiOMPSequential::validation() {
 
 bool SparseMatrixComplexMultiOMPSequential::pre_processing() {
   internal_order_test();
-
   auto* mat1 = reinterpret_cast<Complex*>(taskData->inputs[0]);
   auto* mat2 = reinterpret_cast<Complex*>(taskData->inputs[1]);
 
