f1dcefb44c26edb119c78ed36325a21588375ac3
Frenetz <xodyrev.fedya@mail.ru>
2024-04-23 22:44:52 +0300

    fix

--- tasks/omp/khodyrev_f_convex_hull/src/ops_omp.cpp
+++ tasks/omp/khodyrev_f_convex_hull/src/ops_omp.cpp
@@ -109,7 +109,7 @@ bool khodyrev_omp::KhodyrevTaskOMP::run() {
       Pixel p = hull.top();
       hull.pop();
       result[p.y * width_out + p.x] = 1;
-    }  
+    }
   } catch (const std::exception& e) {
     std::cout << e.what() << std::endl;
     return false;
