b7accd9a43858a8abbee52dbf8d346d8f975b208
Frenetz <xodyrev.fedya@mail.ru>
2024-04-23 20:56:21 +0300

    11

--- tasks/omp/khodyrev_f_convex_hull/src/ops_omp.cpp
+++ tasks/omp/khodyrev_f_convex_hull/src/ops_omp.cpp
@@ -205,7 +205,8 @@ bool khodyrev_omp::KhodyrevTaskSequential::run() {
       hull.pop();
       result[p.y * width_out + p.x] = 1;
     }
-  } catch (const std::exception& e) {
+  } 
+  catch (const std::exception& e) {
     std::cout << e.what() << std::endl;
     return false;
   }
