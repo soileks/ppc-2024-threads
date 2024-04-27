cdb921ad20a9ea6357d1ad70e80a4cbd239ba01c
Valeria <lerasavotina@gmail.com>
2024-04-28 01:51:35 +0300

    first commit

---
+++ tasks/omp/savotina_v_grahams_alg/func_tests/main.cpp
@@ -0,0 +1,314 @@
+// Copyright 2024 Savotina Valeria
+
+#include <gtest/gtest.h>
+
+#include "omp/savotina_v_grahams_alg/include/ops_omp.hpp"
+
+// Points are arranged in a chaotic order (type of x and y is int)
+TEST(savotina_v_grahams_alg_omp, Test1) {
+  // Create data
+  std::vector<SavotinaPoint> points = {SavotinaPoint(-8, 4),   SavotinaPoint(-4, 6),   SavotinaPoint(-12, 2),
+                                       SavotinaPoint(-6, -2),  SavotinaPoint(-10, -4), SavotinaPoint(-4, 2),
+                                       SavotinaPoint(-6, 6),   SavotinaPoint(-8, 8),   SavotinaPoint(-10, 6),
+                                       SavotinaPoint(-8, 2),   SavotinaPoint(-6, 2),   SavotinaPoint(-10, 0),
+                                       SavotinaPoint(-14, -2), SavotinaPoint(-16, -4), SavotinaPoint(-14, -4)};
+  std::vector<SavotinaPoint> mchSeq(7);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> dataGrahamsAlgSeq = std::make_shared<ppc::core::TaskData>();
+  dataGrahamsAlgSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(points.data()));
+  dataGrahamsAlgSeq->inputs_count.emplace_back(points.size());
+  dataGrahamsAlgSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(mchSeq.data()));
+  dataGrahamsAlgSeq->outputs_count.emplace_back(mchSeq.size());
+
+  // Create Task
+  SavotinaOmp::SavotinaGrahamsAlgorithmSequential testGrahamsAlgSeq(dataGrahamsAlgSeq);
+  ASSERT_EQ(testGrahamsAlgSeq.validation(), true);
+  testGrahamsAlgSeq.pre_processing();
+  testGrahamsAlgSeq.run();
+  testGrahamsAlgSeq.post_processing();
+
+  // Create data
+  std::vector<SavotinaPoint> mchOmp(7);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> dataGrahamsAlgOmp = std::make_shared<ppc::core::TaskData>();
+  dataGrahamsAlgOmp->inputs.emplace_back(reinterpret_cast<uint8_t *>(points.data()));
+  dataGrahamsAlgOmp->inputs_count.emplace_back(points.size());
+  dataGrahamsAlgOmp->outputs.emplace_back(reinterpret_cast<uint8_t *>(mchOmp.data()));
+  dataGrahamsAlgOmp->outputs_count.emplace_back(mchOmp.size());
+
+  // Create Task
+  SavotinaOmp::SavotinaGrahamsAlgorithmOmpParallel testGrahamsAlgOmp(dataGrahamsAlgOmp);
+  ASSERT_EQ(testGrahamsAlgOmp.validation(), true);
+  testGrahamsAlgOmp.pre_processing();
+  testGrahamsAlgOmp.run();
+  testGrahamsAlgOmp.post_processing();
+
+  for (size_t i = 0; i < mchSeq.size(); ++i) {
+    ASSERT_EQ(mchSeq[i], mchOmp[i]);
+  }
+}
+
+// Points are arranged in a chaotic order (type of x and y is double)
+TEST(savotina_v_grahams_alg_omp, Test2) {
+  // Create data
+  std::vector<SavotinaPoint> points = {
+      SavotinaPoint(-0.5, 2.2),  SavotinaPoint(1.6, 1.3),  SavotinaPoint(0.3, -0.6),  SavotinaPoint(0.1, 1.3),
+      SavotinaPoint(-1.8, 1.42), SavotinaPoint(-0.3, 0.6), SavotinaPoint(-0.5, -1.2), SavotinaPoint(1.2, -0.8),
+      SavotinaPoint(0.7, 0.4),   SavotinaPoint(1.1, 1.9),  SavotinaPoint(0.4, -1.2),  SavotinaPoint(-1.9, 0.4),
+      SavotinaPoint(-0.6, -0.3), SavotinaPoint(1.8, 0.5),  SavotinaPoint(-1.4, -0.7), SavotinaPoint(-0.9, 1.1),
+      SavotinaPoint(-1.2, 1.9),  SavotinaPoint(0.4, 2.2),  SavotinaPoint(1.7, -0.1)};
+
+  std::vector<SavotinaPoint> mchSeq(13);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> dataGrahamsAlgSeq = std::make_shared<ppc::core::TaskData>();
+  dataGrahamsAlgSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(points.data()));
+  dataGrahamsAlgSeq->inputs_count.emplace_back(points.size());
+  dataGrahamsAlgSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(mchSeq.data()));
+  dataGrahamsAlgSeq->outputs_count.emplace_back(mchSeq.size());
+
+  // Create Task
+  SavotinaOmp::SavotinaGrahamsAlgorithmSequential testGrahamsAlgSeq(dataGrahamsAlgSeq);
+  ASSERT_EQ(testGrahamsAlgSeq.validation(), true);
+  testGrahamsAlgSeq.pre_processing();
+  testGrahamsAlgSeq.run();
+  testGrahamsAlgSeq.post_processing();
+
+  // Create data
+  std::vector<SavotinaPoint> mchOmp(13);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> dataGrahamsAlgOmp = std::make_shared<ppc::core::TaskData>();
+  dataGrahamsAlgOmp->inputs.emplace_back(reinterpret_cast<uint8_t *>(points.data()));
+  dataGrahamsAlgOmp->inputs_count.emplace_back(points.size());
+  dataGrahamsAlgOmp->outputs.emplace_back(reinterpret_cast<uint8_t *>(mchOmp.data()));
+  dataGrahamsAlgOmp->outputs_count.emplace_back(mchOmp.size());
+
+  // Create Task
+  SavotinaOmp::SavotinaGrahamsAlgorithmOmpParallel testGrahamsAlgOmp(dataGrahamsAlgOmp);
+  ASSERT_EQ(testGrahamsAlgOmp.validation(), true);
+  testGrahamsAlgOmp.pre_processing();
+  testGrahamsAlgOmp.run();
+  testGrahamsAlgOmp.post_processing();
+
+  for (size_t i = 0; i < mchSeq.size(); ++i) {
+    ASSERT_EQ(mchSeq[i], mchOmp[i]);
+  }
+}
+
+// Points are located on the same straight line
+TEST(savotina_v_grahams_alg_omp, Test3) {
+  // Create data
+  std::vector<SavotinaPoint> points = {SavotinaPoint(-1.4, 2.2), SavotinaPoint(2.2, 2.2),  SavotinaPoint(-0.6, 2.2),
+                                       SavotinaPoint(-2.3, 2.2), SavotinaPoint(2.7, 2.2),  SavotinaPoint(0.6, 2.2),
+                                       SavotinaPoint(1.7, 2.2),  SavotinaPoint(-3.4, 2.2), SavotinaPoint(5.3, 2.2),
+                                       SavotinaPoint(4.4, 2.2),  SavotinaPoint(3.4, 2.2)};
+  std::vector<SavotinaPoint> mchSeq(2);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> dataGrahamsAlgSeq = std::make_shared<ppc::core::TaskData>();
+  dataGrahamsAlgSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(points.data()));
+  dataGrahamsAlgSeq->inputs_count.emplace_back(points.size());
+  dataGrahamsAlgSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(mchSeq.data()));
+  dataGrahamsAlgSeq->outputs_count.emplace_back(mchSeq.size());
+
+  // Create Task
+  SavotinaOmp::SavotinaGrahamsAlgorithmSequential testGrahamsAlgSeq(dataGrahamsAlgSeq);
+  ASSERT_EQ(testGrahamsAlgSeq.validation(), true);
+  testGrahamsAlgSeq.pre_processing();
+  testGrahamsAlgSeq.run();
+  testGrahamsAlgSeq.post_processing();
+
+  // Create data
+  std::vector<SavotinaPoint> mchOmp(2);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> dataGrahamsAlgOmp = std::make_shared<ppc::core::TaskData>();
+  dataGrahamsAlgOmp->inputs.emplace_back(reinterpret_cast<uint8_t *>(points.data()));
+  dataGrahamsAlgOmp->inputs_count.emplace_back(points.size());
+  dataGrahamsAlgOmp->outputs.emplace_back(reinterpret_cast<uint8_t *>(mchOmp.data()));
+  dataGrahamsAlgOmp->outputs_count.emplace_back(mchOmp.size());
+
+  // Create Task
+  SavotinaOmp::SavotinaGrahamsAlgorithmOmpParallel testGrahamsAlgOmp(dataGrahamsAlgOmp);
+  ASSERT_EQ(testGrahamsAlgOmp.validation(), true);
+  testGrahamsAlgOmp.pre_processing();
+  testGrahamsAlgOmp.run();
+  testGrahamsAlgOmp.post_processing();
+
+  for (size_t i = 0; i < mchSeq.size(); ++i) {
+    ASSERT_EQ(mchSeq[i], mchOmp[i]);
+  }
+}
+
+// Only one point
+TEST(savotina_v_grahams_alg_omp, Test4) {
+  // Create data
+  std::vector<SavotinaPoint> points = {SavotinaPoint(3.5, 4.7)};
+  std::vector<SavotinaPoint> mchSeq(1);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> dataGrahamsAlgSeq = std::make_shared<ppc::core::TaskData>();
+  dataGrahamsAlgSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(points.data()));
+  dataGrahamsAlgSeq->inputs_count.emplace_back(points.size());
+  dataGrahamsAlgSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(mchSeq.data()));
+  dataGrahamsAlgSeq->outputs_count.emplace_back(mchSeq.size());
+
+  // Create Task
+  SavotinaOmp::SavotinaGrahamsAlgorithmSequential testGrahamsAlgSeq(dataGrahamsAlgSeq);
+  ASSERT_EQ(testGrahamsAlgSeq.validation(), true);
+  testGrahamsAlgSeq.pre_processing();
+  testGrahamsAlgSeq.run();
+  testGrahamsAlgSeq.post_processing();
+
+  // Create data
+  std::vector<SavotinaPoint> mchOmp(1);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> dataGrahamsAlgOmp = std::make_shared<ppc::core::TaskData>();
+  dataGrahamsAlgOmp->inputs.emplace_back(reinterpret_cast<uint8_t *>(points.data()));
+  dataGrahamsAlgOmp->inputs_count.emplace_back(points.size());
+  dataGrahamsAlgOmp->outputs.emplace_back(reinterpret_cast<uint8_t *>(mchOmp.data()));
+  dataGrahamsAlgOmp->outputs_count.emplace_back(mchOmp.size());
+
+  // Create Task
+  SavotinaOmp::SavotinaGrahamsAlgorithmOmpParallel testGrahamsAlgOmp(dataGrahamsAlgOmp);
+  ASSERT_EQ(testGrahamsAlgOmp.validation(), true);
+  testGrahamsAlgOmp.pre_processing();
+  testGrahamsAlgOmp.run();
+  testGrahamsAlgOmp.post_processing();
+
+  for (size_t i = 0; i < mchSeq.size(); ++i) {
+    ASSERT_EQ(mchSeq[i], mchOmp[i]);
+  }
+}
+
+// Only two points
+TEST(savotina_v_grahams_alg_omp, Test5) {
+  // Create data
+  std::vector<SavotinaPoint> points = {SavotinaPoint(2.4, -2.9), SavotinaPoint(-1.8, 4.2)};
+  std::vector<SavotinaPoint> mchSeq(2);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> dataGrahamsAlgSeq = std::make_shared<ppc::core::TaskData>();
+  dataGrahamsAlgSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(points.data()));
+  dataGrahamsAlgSeq->inputs_count.emplace_back(points.size());
+  dataGrahamsAlgSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(mchSeq.data()));
+  dataGrahamsAlgSeq->outputs_count.emplace_back(mchSeq.size());
+
+  // Create Task
+  SavotinaOmp::SavotinaGrahamsAlgorithmSequential testGrahamsAlgSeq(dataGrahamsAlgSeq);
+  ASSERT_EQ(testGrahamsAlgSeq.validation(), true);
+  testGrahamsAlgSeq.pre_processing();
+  testGrahamsAlgSeq.run();
+  testGrahamsAlgSeq.post_processing();
+
+  // Create data
+  std::vector<SavotinaPoint> mchOmp(2);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> dataGrahamsAlgOmp = std::make_shared<ppc::core::TaskData>();
+  dataGrahamsAlgOmp->inputs.emplace_back(reinterpret_cast<uint8_t *>(points.data()));
+  dataGrahamsAlgOmp->inputs_count.emplace_back(points.size());
+  dataGrahamsAlgOmp->outputs.emplace_back(reinterpret_cast<uint8_t *>(mchOmp.data()));
+  dataGrahamsAlgOmp->outputs_count.emplace_back(mchOmp.size());
+
+  // Create Task
+  SavotinaOmp::SavotinaGrahamsAlgorithmOmpParallel testGrahamsAlgOmp(dataGrahamsAlgOmp);
+  ASSERT_EQ(testGrahamsAlgOmp.validation(), true);
+  testGrahamsAlgOmp.pre_processing();
+  testGrahamsAlgOmp.run();
+  testGrahamsAlgOmp.post_processing();
+
+  for (size_t i = 0; i < mchSeq.size(); ++i) {
+    ASSERT_EQ(mchSeq[i], mchOmp[i]);
+  }
+}
+
+// No points
+TEST(savotina_v_grahams_alg_omp, Test6) {
+  // Create data
+  std::vector<SavotinaPoint> points = {};
+  std::vector<SavotinaPoint> mchSeq(points.size());
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> dataGrahamsAlgSeq = std::make_shared<ppc::core::TaskData>();
+  dataGrahamsAlgSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(points.data()));
+  dataGrahamsAlgSeq->inputs_count.emplace_back(points.size());
+  dataGrahamsAlgSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(mchSeq.data()));
+  dataGrahamsAlgSeq->outputs_count.emplace_back(mchSeq.size());
+
+  // Create Task
+  SavotinaOmp::SavotinaGrahamsAlgorithmSequential testGrahamsAlgSeq(dataGrahamsAlgSeq);
+  ASSERT_EQ(testGrahamsAlgSeq.validation(), true);
+  testGrahamsAlgSeq.pre_processing();
+  testGrahamsAlgSeq.run();
+  testGrahamsAlgSeq.post_processing();
+
+  // Create data
+  std::vector<SavotinaPoint> mchOmp(points.size());
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> dataGrahamsAlgOmp = std::make_shared<ppc::core::TaskData>();
+  dataGrahamsAlgOmp->inputs.emplace_back(reinterpret_cast<uint8_t *>(points.data()));
+  dataGrahamsAlgOmp->inputs_count.emplace_back(points.size());
+  dataGrahamsAlgOmp->outputs.emplace_back(reinterpret_cast<uint8_t *>(mchOmp.data()));
+  dataGrahamsAlgOmp->outputs_count.emplace_back(mchOmp.size());
+
+  // Create Task
+  SavotinaOmp::SavotinaGrahamsAlgorithmOmpParallel testGrahamsAlgOmp(dataGrahamsAlgOmp);
+  ASSERT_EQ(testGrahamsAlgOmp.validation(), true);
+  testGrahamsAlgOmp.pre_processing();
+  testGrahamsAlgOmp.run();
+  testGrahamsAlgOmp.post_processing();
+
+  for (size_t i = 0; i < mchSeq.size(); ++i) {
+    ASSERT_EQ(mchSeq[i], mchOmp[i]);
+  }
+}
+
+TEST(savotina_v_grahams_alg_omp, Test7) {
+  // Create data
+  std::vector<SavotinaPoint> points = {SavotinaPoint(4.4, 1.7), SavotinaPoint(4.4, 1.7), SavotinaPoint(4.4, 1.7),
+                                       SavotinaPoint(4.4, 1.7), SavotinaPoint(4.4, 1.7), SavotinaPoint(4.4, 1.7),
+                                       SavotinaPoint(4.4, 1.7), SavotinaPoint(4.4, 1.7), SavotinaPoint(4.4, 1.7),
+                                       SavotinaPoint(4.4, 1.7)};
+  std::vector<SavotinaPoint> mchSeq(1);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> dataGrahamsAlgSeq = std::make_shared<ppc::core::TaskData>();
+  dataGrahamsAlgSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(points.data()));
+  dataGrahamsAlgSeq->inputs_count.emplace_back(points.size());
+  dataGrahamsAlgSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(mchSeq.data()));
+  dataGrahamsAlgSeq->outputs_count.emplace_back(mchSeq.size());
+
+  // Create Task
+  SavotinaOmp::SavotinaGrahamsAlgorithmSequential testGrahamsAlgSeq(dataGrahamsAlgSeq);
+  ASSERT_EQ(testGrahamsAlgSeq.validation(), true);
+  testGrahamsAlgSeq.pre_processing();
+  testGrahamsAlgSeq.run();
+  testGrahamsAlgSeq.post_processing();
+
+  // Create data
+  std::vector<SavotinaPoint> mchOmp(1);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> dataGrahamsAlgOmp = std::make_shared<ppc::core::TaskData>();
+  dataGrahamsAlgOmp->inputs.emplace_back(reinterpret_cast<uint8_t *>(points.data()));
+  dataGrahamsAlgOmp->inputs_count.emplace_back(points.size());
+  dataGrahamsAlgOmp->outputs.emplace_back(reinterpret_cast<uint8_t *>(mchOmp.data()));
+  dataGrahamsAlgOmp->outputs_count.emplace_back(mchOmp.size());
+
+  // Create Task
+  SavotinaOmp::SavotinaGrahamsAlgorithmOmpParallel testGrahamsAlgOmp(dataGrahamsAlgOmp);
+  ASSERT_EQ(testGrahamsAlgOmp.validation(), true);
+  testGrahamsAlgOmp.pre_processing();
+  testGrahamsAlgOmp.run();
+  testGrahamsAlgOmp.post_processing();
+
+  for (size_t i = 0; i < mchSeq.size(); ++i) {
+    ASSERT_EQ(mchSeq[i], mchOmp[i]);
+  }
+}
---
+++ tasks/omp/savotina_v_grahams_alg/perf_tests/main.cpp
@@ -0,0 +1,90 @@
+// Copyright 2024 Savotina Valeria
+
+#include <gtest/gtest.h>
+
+#include "core/perf/include/perf.hpp"
+#include "omp/savotina_v_grahams_alg/include/ops_omp.hpp"
+
+TEST(savotina_v_grahams_alg_omp, test_pipeline_run) {
+  double min = 0;
+  double max = 10000;
+  size_t size = 2000000;  // size of vector of random points
+
+  // Create data
+  std::vector<SavotinaPoint> points = {SavotinaPoint(max, min), SavotinaPoint(min, max), SavotinaPoint(max, max),
+                                       SavotinaPoint(min, min)};
+  std::vector<SavotinaPoint> randomPoints = SavotinaOmp::SavotinaRandomPoints(min, max, size);
+  points.insert(points.end(), randomPoints.begin(), randomPoints.end());
+  std::vector<SavotinaPoint> res = {SavotinaPoint(min, min), SavotinaPoint(max, min), SavotinaPoint(max, max),
+                                    SavotinaPoint(min, max)};
+  std::vector<SavotinaPoint> mch(res.size());
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> dataGrahamsAlgOmp = std::make_shared<ppc::core::TaskData>();
+  dataGrahamsAlgOmp->inputs.emplace_back(reinterpret_cast<uint8_t *>(points.data()));
+  dataGrahamsAlgOmp->inputs_count.emplace_back(points.size());
+  dataGrahamsAlgOmp->outputs.emplace_back(reinterpret_cast<uint8_t *>(mch.data()));
+  dataGrahamsAlgOmp->outputs_count.emplace_back(mch.size());
+
+  // Create Task
+  auto testGrahamOmp = std::make_shared<SavotinaOmp::SavotinaGrahamsAlgorithmOmpParallel>(dataGrahamsAlgOmp);
+
+  // Create Perf attributes
+  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+  perfAttr->num_running = 10;
+  perfAttr->current_timer = [&] { return omp_get_wtime(); };
+
+  // Create and init perf results
+  auto perfResults = std::make_shared<ppc::core::PerfResults>();
+
+  // Create Perf analyzer
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testGrahamOmp);
+  perfAnalyzer->pipeline_run(perfAttr, perfResults);
+  ppc::core::Perf::print_perf_statistic(perfResults);
+
+  for (size_t i = 0; i < res.size(); ++i) {
+    ASSERT_EQ(mch[i], res[i]);
+  }
+}
+
+TEST(savotina_v_grahams_alg_omp, test_task_run) {
+  double min = 0;
+  double max = 10000;
+  size_t size = 2000000;  // size of vector of random points
+
+  // Create data
+  std::vector<SavotinaPoint> points = {SavotinaPoint(max, min), SavotinaPoint(min, max), SavotinaPoint(max, max),
+                                       SavotinaPoint(min, min)};
+  std::vector<SavotinaPoint> randomPoints = SavotinaOmp::SavotinaRandomPoints(min, max, size);
+  points.insert(points.end(), randomPoints.begin(), randomPoints.end());
+  std::vector<SavotinaPoint> res = {SavotinaPoint(min, min), SavotinaPoint(max, min), SavotinaPoint(max, max),
+                                    SavotinaPoint(min, max)};
+  std::vector<SavotinaPoint> mch(res.size());
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> dataGrahamsAlgOmp = std::make_shared<ppc::core::TaskData>();
+  dataGrahamsAlgOmp->inputs.emplace_back(reinterpret_cast<uint8_t *>(points.data()));
+  dataGrahamsAlgOmp->inputs_count.emplace_back(points.size());
+  dataGrahamsAlgOmp->outputs.emplace_back(reinterpret_cast<uint8_t *>(mch.data()));
+  dataGrahamsAlgOmp->outputs_count.emplace_back(mch.size());
+
+  // Create Task
+  auto testGrahamOmp = std::make_shared<SavotinaOmp::SavotinaGrahamsAlgorithmOmpParallel>(dataGrahamsAlgOmp);
+
+  // Create Perf attributes
+  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+  perfAttr->num_running = 10;
+  perfAttr->current_timer = [&] { return omp_get_wtime(); };
+
+  // Create and init perf results
+  auto perfResults = std::make_shared<ppc::core::PerfResults>();
+
+  // Create Perf analyzer
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testGrahamOmp);
+  perfAnalyzer->task_run(perfAttr, perfResults);
+  ppc::core::Perf::print_perf_statistic(perfResults);
+
+  for (size_t i = 0; i < res.size(); ++i) {
+    ASSERT_EQ(mch[i], res[i]);
+  }
+}
