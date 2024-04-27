6ac95360f2aeb670835405d2eb91252f587c9766
Данила Ермолаев <73434195+Dnoniel-Ermolaev@users.noreply.github.com>
2024-04-27 15:54:28 +0300

    first try

---
+++ tasks/seq/ermolaev_d_fox_algorithm/func_tests/main.cpp
@@ -0,0 +1,289 @@
+#include <gtest/gtest.h>
+
+#include <random>
+#include <vector>
+
+#include "seq/ermolaev_d_fox_algorithm/include/ops_seq.hpp"
+
+TEST(ermolaev_d_fox_algorithm_seq, Test_Matrix_Multiplication_Simple_10) {
+  constexpr size_t matrix_size = 10;
+  std::vector<double> A(matrix_size * matrix_size, 1.0);
+  std::vector<double> B(matrix_size * matrix_size, 2.0);
+  std::vector<double> C(matrix_size * matrix_size, 0.0);
+  std::vector<double> expected(matrix_size * matrix_size, matrix_size * 2.0);
+
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(A.data()));
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(B.data()));
+  taskDataSeq->inputs_count.emplace_back(matrix_size * matrix_size);
+  taskDataSeq->inputs_count.emplace_back(matrix_size * matrix_size);
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(C.data()));
+  taskDataSeq->outputs_count.emplace_back(matrix_size * matrix_size);
+
+  FoxAlgorithm test(taskDataSeq);
+  ASSERT_TRUE(test.validation());
+  test.pre_processing();
+  test.run();
+  test.post_processing();
+
+  for (size_t i = 0; i < matrix_size * matrix_size; i++) {
+    ASSERT_DOUBLE_EQ(C[i], expected[i]);
+  }
+}
+
+TEST(ermolaev_d_fox_algorithm_seq, Test_Matrix_Multiplication_Random_10) {
+  constexpr size_t matrix_size = 10;
+  std::mt19937 gen(1);  // Инициализация генератора случайных чисел с начальным значением 1
+  std::uniform_real_distribution<> dis(5.0, 1e-5);
+  std::vector<double> A(matrix_size * matrix_size);
+  std::vector<double> B(matrix_size * matrix_size);
+  std::vector<double> C(matrix_size * matrix_size, 0.0);
+  std::vector<double> expected(matrix_size * matrix_size);
+
+  // Заполнение матриц A и B случайными значениями
+  for (size_t i = 0; i < matrix_size * matrix_size; ++i) {
+    double random_value = dis(gen);  // Генерация случайного числа
+    A[i] = B[i] = random_value;      // Запись случайного числа в матрицы A и B
+  }
+
+  // Расчет ожидаемого результата
+  for (size_t i = 0; i < matrix_size; ++i) {
+    for (size_t j = 0; j < matrix_size; ++j) {
+      for (size_t k = 0; k < matrix_size; ++k) {
+        expected[i * matrix_size + j] += A[i * matrix_size + k] * B[k * matrix_size + j];
+      }
+    }
+  }
+
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(A.data()));
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(B.data()));
+  taskDataSeq->inputs_count.emplace_back(matrix_size * matrix_size);
+  taskDataSeq->inputs_count.emplace_back(matrix_size * matrix_size);
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(C.data()));
+  taskDataSeq->outputs_count.emplace_back(matrix_size * matrix_size);
+
+  FoxAlgorithm test(taskDataSeq);
+  ASSERT_TRUE(test.validation());
+  test.pre_processing();
+  test.run();
+  test.post_processing();
+
+  // Проверка, что результат умножения соответствует ожидаемому результату
+  for (size_t i = 0; i < matrix_size * matrix_size; ++i) {
+    ASSERT_DOUBLE_EQ(C[i], expected[i]);
+  }
+}
+
+TEST(ermolaev_d_fox_algorithm_seq, Test_Matrix_Multiplication_Simple_50) {
+  constexpr size_t matrix_size = 50;
+  std::vector<double> A(matrix_size * matrix_size, 1.0);
+  std::vector<double> B(matrix_size * matrix_size, 2.0);
+  std::vector<double> C(matrix_size * matrix_size, 0.0);
+  std::vector<double> expected(matrix_size * matrix_size, matrix_size * 2.0);
+
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(A.data()));
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(B.data()));
+  taskDataSeq->inputs_count.emplace_back(matrix_size * matrix_size);
+  taskDataSeq->inputs_count.emplace_back(matrix_size * matrix_size);
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(C.data()));
+  taskDataSeq->outputs_count.emplace_back(matrix_size * matrix_size);
+
+  FoxAlgorithm test(taskDataSeq);
+  ASSERT_TRUE(test.validation());
+  test.pre_processing();
+  test.run();
+  test.post_processing();
+
+  for (size_t i = 0; i < matrix_size * matrix_size; i++) {
+    ASSERT_DOUBLE_EQ(C[i], expected[i]);
+  }
+}
+
+TEST(ermolaev_d_fox_algorithm_seq, Test_Matrix_Multiplication_Random_50) {
+  constexpr size_t matrix_size = 50;
+  std::mt19937 gen(1);  // Инициализация генератора случайных чисел с начальным значением 1
+  const double tolerance = 1e-5;
+  std::uniform_real_distribution<> dis(5.0, 1e-5);
+  std::vector<double> A(matrix_size * matrix_size);
+  std::vector<double> B(matrix_size * matrix_size);
+  std::vector<double> C(matrix_size * matrix_size, 0.0);
+  std::vector<double> expected(matrix_size * matrix_size);
+
+  // Заполнение матриц A и B случайными значениями
+  for (size_t i = 0; i < matrix_size * matrix_size; ++i) {
+    double random_value = dis(gen);  // Генерация случайного числа
+    A[i] = B[i] = random_value;      // Запись случайного числа в матрицы A и B
+  }
+
+  // Расчет ожидаемого результата
+  for (size_t i = 0; i < matrix_size; ++i) {
+    for (size_t j = 0; j < matrix_size; ++j) {
+      for (size_t k = 0; k < matrix_size; ++k) {
+        expected[i * matrix_size + j] += A[i * matrix_size + k] * B[k * matrix_size + j];
+      }
+    }
+  }
+
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(A.data()));
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(B.data()));
+  taskDataSeq->inputs_count.emplace_back(matrix_size * matrix_size);
+  taskDataSeq->inputs_count.emplace_back(matrix_size * matrix_size);
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(C.data()));
+  taskDataSeq->outputs_count.emplace_back(matrix_size * matrix_size);
+
+  FoxAlgorithm test(taskDataSeq);
+  ASSERT_TRUE(test.validation());
+  test.pre_processing();
+  test.run();
+  test.post_processing();
+
+  // Проверка, что результат умножения соответствует ожидаемому результату
+  for (size_t i = 0; i < matrix_size * matrix_size; ++i) {
+    ASSERT_NEAR(C[i], expected[i], tolerance);
+  }
+}
+
+TEST(ermolaev_d_fox_algorithm_seq, Test_Matrix_Multiplication_Simple_75) {
+  constexpr size_t matrix_size = 75;
+  std::vector<double> A(matrix_size * matrix_size, 1.0);
+  std::vector<double> B(matrix_size * matrix_size, 2.0);
+  std::vector<double> C(matrix_size * matrix_size, 0.0);
+  std::vector<double> expected(matrix_size * matrix_size, matrix_size * 2.0);
+
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(A.data()));
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(B.data()));
+  taskDataSeq->inputs_count.emplace_back(matrix_size * matrix_size);
+  taskDataSeq->inputs_count.emplace_back(matrix_size * matrix_size);
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(C.data()));
+  taskDataSeq->outputs_count.emplace_back(matrix_size * matrix_size);
+
+  FoxAlgorithm test(taskDataSeq);
+  ASSERT_TRUE(test.validation());
+  test.pre_processing();
+  test.run();
+  test.post_processing();
+
+  for (size_t i = 0; i < matrix_size * matrix_size; i++) {
+    ASSERT_DOUBLE_EQ(C[i], expected[i]);
+  }
+}
+
+TEST(ermolaev_d_fox_algorithm_seq, Test_Matrix_Multiplication_Random_75) {
+  constexpr size_t matrix_size = 75;
+  const double tolerance = 1e-5;
+  std::mt19937 gen(1);  // Инициализация генератора случайных чисел с начальным значением 1
+  std::uniform_real_distribution<> dis(5.0, 1e-5);
+  std::vector<double> A(matrix_size * matrix_size);
+  std::vector<double> B(matrix_size * matrix_size);
+  std::vector<double> C(matrix_size * matrix_size, 0.0);
+  std::vector<double> expected(matrix_size * matrix_size);
+
+  // Заполнение матриц A и B случайными значениями
+  for (size_t i = 0; i < matrix_size * matrix_size; ++i) {
+    double random_value = dis(gen);  // Генерация случайного числа
+    A[i] = B[i] = random_value;      // Запись случайного числа в матрицы A и B
+  }
+
+  // Расчет ожидаемого результата
+  for (size_t i = 0; i < matrix_size; ++i) {
+    for (size_t j = 0; j < matrix_size; ++j) {
+      for (size_t k = 0; k < matrix_size; ++k) {
+        expected[i * matrix_size + j] += A[i * matrix_size + k] * B[k * matrix_size + j];
+      }
+    }
+  }
+
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(A.data()));
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(B.data()));
+  taskDataSeq->inputs_count.emplace_back(matrix_size * matrix_size);
+  taskDataSeq->inputs_count.emplace_back(matrix_size * matrix_size);
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(C.data()));
+  taskDataSeq->outputs_count.emplace_back(matrix_size * matrix_size);
+
+  FoxAlgorithm test(taskDataSeq);
+  ASSERT_TRUE(test.validation());
+  test.pre_processing();
+  test.run();
+  test.post_processing();
+
+  // Проверка, что результат умножения соответствует ожидаемому результату
+  for (size_t i = 0; i < matrix_size * matrix_size; ++i) {
+    ASSERT_NEAR(C[i], expected[i], tolerance);
+  }
+}
+
+TEST(ermolaev_d_fox_algorithm_seq, Test_Matrix_Multiplication_Simple_80) {
+  constexpr size_t matrix_size = 80;
+  std::vector<double> A(matrix_size * matrix_size, 1.0);
+  std::vector<double> B(matrix_size * matrix_size, 2.0);
+  std::vector<double> C(matrix_size * matrix_size, 0.0);
+  std::vector<double> expected(matrix_size * matrix_size, matrix_size * 2.0);
+
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(A.data()));
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(B.data()));
+  taskDataSeq->inputs_count.emplace_back(matrix_size * matrix_size);
+  taskDataSeq->inputs_count.emplace_back(matrix_size * matrix_size);
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(C.data()));
+  taskDataSeq->outputs_count.emplace_back(matrix_size * matrix_size);
+
+  FoxAlgorithm test(taskDataSeq);
+  ASSERT_TRUE(test.validation());
+  test.pre_processing();
+  test.run();
+  test.post_processing();
+
+  for (size_t i = 0; i < matrix_size * matrix_size; i++) {
+    ASSERT_DOUBLE_EQ(C[i], expected[i]);
+  }
+}
+
+TEST(ermolaev_d_fox_algorithm_seq, Test_Matrix_Multiplication_Random_80) {
+  constexpr size_t matrix_size = 80;
+  const double tolerance = 1e-5;
+  std::mt19937 gen(1);  // Инициализация генератора случайных чисел с начальным значением 1
+  std::uniform_real_distribution<> dis(5.0, 1e-5);
+  std::vector<double> A(matrix_size * matrix_size);
+  std::vector<double> B(matrix_size * matrix_size);
+  std::vector<double> C(matrix_size * matrix_size, 0.0);
+  std::vector<double> expected(matrix_size * matrix_size);
+
+  // Заполнение матриц A и B случайными значениями
+  for (size_t i = 0; i < matrix_size * matrix_size; ++i) {
+    double random_value = dis(gen);  // Генерация случайного числа
+    A[i] = B[i] = random_value;      // Запись случайного числа в матрицы A и B
+  }
+
+  // Расчет ожидаемого результата
+  for (size_t i = 0; i < matrix_size; ++i) {
+    for (size_t j = 0; j < matrix_size; ++j) {
+      for (size_t k = 0; k < matrix_size; ++k) {
+        expected[i * matrix_size + j] += A[i * matrix_size + k] * B[k * matrix_size + j];
+      }
+    }
+  }
+
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(A.data()));
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(B.data()));
+  taskDataSeq->inputs_count.emplace_back(matrix_size * matrix_size);
+  taskDataSeq->inputs_count.emplace_back(matrix_size * matrix_size);
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(C.data()));
+  taskDataSeq->outputs_count.emplace_back(matrix_size * matrix_size);
+
+  FoxAlgorithm test(taskDataSeq);
+  ASSERT_TRUE(test.validation());
+  test.pre_processing();
+  test.run();
+  test.post_processing();
+
+  // Проверка, что результат умножения соответствует ожидаемому результату
+  for (size_t i = 0; i < matrix_size * matrix_size; ++i) {
+    ASSERT_NEAR(C[i], expected[i], tolerance);
+  }
+}
