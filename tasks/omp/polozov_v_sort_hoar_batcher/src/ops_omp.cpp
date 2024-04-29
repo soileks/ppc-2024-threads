eb971e9ccf8493407c749885e12b89fb541e484d
Mortus19 <Polozzzzzov2033@mail.ru>
2024-04-29 17:07:11 +0300

    please pass

--- tasks/omp/polozov_v_sort_hoar_batcher/src/ops_omp.cpp
+++ tasks/omp/polozov_v_sort_hoar_batcher/src/ops_omp.cpp
@@ -93,11 +93,12 @@ std::vector<int> odd_even_merge_with_hoar(std::vector<int> my_data) {
       }
     }
   };
-#pragma omp parallel
+#pragma omp parallel num_threads(2)
   {
     if (omp_get_thread_num() == 0) {
       merge(0, n / 2 - 1);
-    } else {
+    }
+    if (omp_get_thread_num() == 1) {
       merge(n / 2, n - 1);
     }
   }
