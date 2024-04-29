bea451d311f3dac4900f31ff7763c86f4a1701d1
Иван Васильев ФИИТ1 <Vanovano03@mail.ru>
2024-04-29 22:58:12 +0300

    do task

---
+++ tasks/omp/Vasilev_i_sort/perf_tests/main.cpp
@@ -0,0 +1,79 @@
+// Copyright 2024 Vasilev Ivan  
+#include <gtest/gtest.h>
+#include <vector>
+#include "core/perf/include/perf.hpp"
+#include "omp/Vasilev_i_sort/include/ops_omp.hpp"
+
+TEST(vasilev_i_sort_omp, pipeline_run) {
+  const int count = 32768;
+
+  // Create data
+  std::vector<int> in = VasilevTaskOmp::generate_random_vector(count, 1, 32768);
+  std::vector<int> out(count, 0);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataOMP = std::make_shared<ppc::core::TaskData>();
+  taskDataOMP->inputs.emplace_back(reinterpret_cast<uint8_t *>(&in));
+  taskDataOMP->inputs_count.emplace_back(in.size());
+  taskDataOMP->outputs.emplace_back(reinterpret_cast<uint8_t *>(&out));
+  taskDataOMP->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  auto testTaskOmp = std::make_shared<VasilevTaskOmp>(taskDataOMP);
+
+  // Create Perf attributes
+  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+  perfAttr->num_running = 10;
+  const auto t0 = std::chrono::high_resolution_clock::now();
+  perfAttr->current_timer = [&] {
+    auto current_time_point = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
+    return static_cast<double>(duration) * 1e-9;
+  };
+
+  // Create and init perf results
+  auto perfResults = std::make_shared<ppc::core::PerfResults>();
+
+  // Create Perf analyzer
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskOmp);
+  perfAnalyzer->pipeline_run(perfAttr, perfResults);
+  ppc::core::Perf::print_perf_statistic(perfResults);
+  ASSERT_TRUE(VasilevTaskOmp::R_sorted(out));
+}
+
+TEST(vasilev_i_sort_omp, task_run) {
+  const int count = 1048576;
+
+  // Create data
+  std::vector<int> in = VasilevTaskOmp::generate_random_vector(count, 1, 1048576);
+  std::vector<int> out(count, 0);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataOMP = std::make_shared<ppc::core::TaskData>();
+  taskDataOMP->inputs.emplace_back(reinterpret_cast<uint8_t *>(&in));
+  taskDataOMP->inputs_count.emplace_back(in.size());
+  taskDataOMP->outputs.emplace_back(reinterpret_cast<uint8_t *>(&out));
+  taskDataOMP->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  auto testTaskOmp = std::make_shared<VasilevTaskOmp>(taskDataOMP);
+
+  // Create Perf attributes
+  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+  perfAttr->num_running = 10;
+  const auto t0 = std::chrono::high_resolution_clock::now();
+  perfAttr->current_timer = [&] {
+    auto current_time_point = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
+    return static_cast<double>(duration) * 1e-9;
+  };
+
+  // Create and init perf results
+  auto perfResults = std::make_shared<ppc::core::PerfResults>();
+
+  // Create Perf analyzer
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskOmp);
+  perfAnalyzer->task_run(perfAttr, perfResults);
+  ppc::core::Perf::print_perf_statistic(perfResults);
+  ASSERT_TRUE(VasilevTaskOmp::R_sorted(out));
+}
---
+++ tasks/omp/Vasilev_i_sort/src/ops_omp.cpp
@@ -0,0 +1,152 @@
+// Copyright 2024 Vasilev Ivan
+// #include <thread>
+
+#include "omp/Vasilev_i_sort/include/ops_omp.hpp"
+
+#include <algorithm>
+// using namespace std::chrono_literals;
+
+bool VasilevTaskOmp::pre_processing() {
+  internal_order_test();
+  // Init value for input and output
+  input_ = *reinterpret_cast<std::vector<int>*>(taskData->inputs[0]);
+  res = *reinterpret_cast<std::vector<int>*>(taskData->outputs[0]);
+  return true;
+}
+
+bool VasilevTaskOmp::validation() {
+  internal_order_test();
+  // Check count elements of output
+  // return taskData->inputs_count[0] == 1 && taskData->outputs_count[0] == 1;
+  return true;
+}
+
+bool VasilevTaskOmp::run() {
+  internal_order_test();
+  shell_sort_batcher(input_, input_.size());
+  return true;
+}
+
+bool VasilevTaskOmp::post_processing() {
+  internal_order_test();
+  // reinterpret_cast<int*>(taskData->outputs[0])[0] = res;
+  reinterpret_cast<std::vector<int>*>(taskData->outputs[0])->operator=(input_);
+
+  return true;
+}
+void VasilevTaskOmp::shell_sort_batcher(std::vector<int>& arr, int n) {
+    for (int gap = n / 2; gap > 0; gap /= 2) {
+        bool sorted = false;
+        while (!sorted) {
+            sorted = true;
+
+            #pragma omp parallel for
+            for (int i = 0; i < n - gap; i += 2) {
+                if (arr[i] > arr[i + gap]) {
+                    std::swap(arr[i], arr[i + gap]);
+                    sorted = false;
+                }
+            }
+
+            #pragma omp parallel for
+            for (int i = 1; i < n - gap; i += 2) {
+                if (arr[i] > arr[i + gap]) {
+                    std::swap(arr[i], arr[i + gap]);
+                    sorted = false;
+                }
+            }
+        }
+    }
+}
+// Функция для слияния отсортированных массивов
+// void VasilevTaskOmp::merge(int l, int m, int r) {
+//   int n1 = m - l + 1;
+//   int n2 = r - m;
+
+//   // создаем временные массивы
+//   std::vector<int> L(n1);
+//   std::vector<int> R(n2);
+
+//   // копируем данные во временные массивы
+//   for (int i = 0; i < n1; i++) {
+//     L[i] = input_[l + i];
+//   }
+//   for (int j = 0; j < n2; j++) {
+//     R[j] = input_[m + 1 + j];
+//   }
+
+//   // слияние временных массивов обратно в основной массив
+//   int i = 0;
+//   int j = 0;
+//   int k = l;
+//   while (i < n1 && j < n2) {
+//     if (L[i] <= R[j]) {
+//       input_[k] = L[i];
+//       i++;
+//     } else {
+//       input_[k] = R[j];
+//       j++;
+//     }
+//     k++;
+//   }
+
+//   // остаток элементов
+//   while (i < n1) {
+//     input_[k] = L[i];
+//     i++;
+//     k++;
+//   }
+//   while (j < n2) {
+//     input_[k] = R[j];
+//     j++;
+//     k++;
+//   }
+// }
+
+// Сортировка Шелла
+// void VasilevTaskOmp::shellSort(std::vector<int>& arr, int n) {
+//    for (int gap = n / 2; gap > 0; gap /= 2) {
+//         #pragma omp parallel for
+//         for (int i = gap; i < n; i += 1) {
+//             int temp = arr[i];
+//             int j;
+//             for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+//                 arr[j] = arr[j - gap];
+//             arr[j] = temp;
+//         }
+//     }
+// }
+
+// Четно-нечетное слияние Бэтчера
+// void VasilevTaskOmp::batcherMerge(int l, int r) {
+//   shellSort();
+//   if (r > l) {
+//     int m = l + (r - l) / 2;
+//     for (int i = l; i <= m; i += 2) {
+//       if (i + 1 <= m) {
+//         if (input_[i] > input_[i + 1]) {
+//           std::swap(input_[i], input_[i + 1]);
+//         }
+//       }
+//     }
+//     batcherMerge(l, m);
+//     batcherMerge(m + 1, r);
+//     merge(l, m, r);
+//   }
+// }
+bool VasilevTaskOmp::R_sorted(std::vector<int> input) { return std::is_sorted(input.begin(), input.end()); }
+
+std::vector<int> VasilevTaskOmp::generate_random_vector(int size, int min, int max) {
+  // First create an instance of an engine.
+  std::random_device rnd_device;
+  // Specify the engine and distribution.
+  std::mt19937 mersenne_engine{rnd_device()};  // Generates random integers
+  std::uniform_int_distribution<int> dist{min, max};
+
+  auto gen = [&dist, &mersenne_engine]() { return dist(mersenne_engine); };
+
+  std::vector<int> vec(size);
+  generate(begin(vec), end(vec), gen);
+
+  return vec;
+}
