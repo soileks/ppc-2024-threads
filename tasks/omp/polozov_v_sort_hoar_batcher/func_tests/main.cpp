10bc995024d19944cfdf7070f5f1d7aab6d81385
Mortus19 <Polozzzzzov2033@mail.ru>
2024-04-29 16:40:02 +0300

    fixed5

--- tasks/omp/polozov_v_sort_hoar_batcher/func_tests/main.cpp
+++ tasks/omp/polozov_v_sort_hoar_batcher/func_tests/main.cpp
@@ -58,8 +58,7 @@ TEST(polozov_v_sort_hoar_batcher_omp, rand_array_16) {
 TEST(polozov_v_sort_hoar_batcher_omp, array_negative_32) {
   // Create data
   std::vector<int> array = generate_data(32, 0, 100);
-  for (int i = 0; i < 32; i++)
-      array[i] *= -1;
+  for (int i = 0; i < 32; i++) array[i] *= -1;
   std::vector<int> out(array.size());
 
   // Create TaskData
