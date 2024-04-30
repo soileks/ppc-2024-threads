34aadba70cff91b10314edfe16ae912c694e50a8
Julia <kurdina.00@gmail.com>
2024-04-30 03:03:20 +0300

    first commit

---
+++ tasks/omp/kurdina_j_linear_filter/include/ops_omp.hpp
@@ -0,0 +1,43 @@
+// Copyright 2024 Kurdina Julia
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+class HorizontalSplitOMP_seq : public ppc::core::Task {
+ public:
+  explicit HorizontalSplitOMP_seq(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+  uint8_t NewColor(int i, int j);
+
+ private:
+  std::vector<uint8_t> input_ = {};
+  std::vector<uint8_t> image = {};
+  std::vector<uint8_t> result_im = {};
+  std::vector<uint8_t> gauss_kernel = {};
+  int n{}, m{};
+  int ker_size = 3;
+};
+
+class HorizontalSplitOMP_parallel : public ppc::core::Task {
+ public:
+  explicit HorizontalSplitOMP_parallel(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+  uint8_t NewColor(int i, int j);
+
+ private:
+  std::vector<uint8_t> input_ = {};
+  std::vector<uint8_t> image = {};
+  std::vector<uint8_t> result_im = {};
+  std::vector<uint8_t> gauss_kernel = {};
+  int n{}, m{};
+  int ker_size = 3;
+};
---
+++ tasks/omp/kurdina_j_linear_filter/perf_tests/main.cpp
@@ -0,0 +1,92 @@
+// Copyright 2024 Kurdina Julia
+#include <gtest/gtest.h>
+#include <omp.h>
+
+#include <vector>
+
+#include "core/perf/include/perf.hpp"
+#include "omp/kurdina_j_linear_filter/include/ops_omp.hpp"
+
+TEST(kurdina_j_linear_filter_omp, test_pipeline_run) {
+  int n = 700;
+  int m = 700;
+  std::vector<uint8_t> gauss_kernel = {1, 1, 1, 1, 1, 1, 1, 1, 1};
+
+  // Create data
+  std::vector<uint8_t> in(n * m, 255);
+  std::vector<int> size_m = {n, m};
+  std::vector<uint8_t> out_parallel(n * m);
+  std::vector<uint8_t> result(n * m, 255);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+  taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  taskDataPar->inputs_count.emplace_back(in.size());
+  taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(size_m.data()));
+  taskDataPar->inputs_count.emplace_back(size_m.size());
+  taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(gauss_kernel.data()));
+  taskDataPar->inputs_count.emplace_back(gauss_kernel.size());
+  taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_parallel.data()));
+  taskDataPar->outputs_count.emplace_back(out_parallel.size());
+
+  // Create Task
+  auto testTaskOMP = std::make_shared<HorizontalSplitOMP_parallel>(taskDataPar);
+
+  // Create Perf attributes
+  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+  perfAttr->num_running = 10;
+  perfAttr->current_timer = [&] { return omp_get_wtime(); };
+
+  // Create and init perf results
+  auto perfResults = std::make_shared<ppc::core::PerfResults>();
+
+  // Create Perf analyzer
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskOMP);
+  perfAnalyzer->pipeline_run(perfAttr, perfResults);
+  ppc::core::Perf::print_perf_statistic(perfResults);
+  for (int i = 0; i < n * m; i++) {
+    ASSERT_EQ(result[i], out_parallel[i]);
+  }
+}
+
+TEST(kurdina_j_linear_filter_omp, test_task_run) {
+  int n = 700;
+  int m = 700;
+  std::vector<uint8_t> gauss_kernel = {1, 1, 1, 1, 1, 1, 1, 1, 1};
+
+  // Create data
+  std::vector<uint8_t> in(n * m, 255);
+  std::vector<int> size_m = {n, m};
+  std::vector<uint8_t> out_parallel(n * m);
+  std::vector<uint8_t> result(n * m, 255);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+  taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  taskDataPar->inputs_count.emplace_back(in.size());
+  taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(size_m.data()));
+  taskDataPar->inputs_count.emplace_back(size_m.size());
+  taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(gauss_kernel.data()));
+  taskDataPar->inputs_count.emplace_back(gauss_kernel.size());
+  taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_parallel.data()));
+  taskDataPar->outputs_count.emplace_back(out_parallel.size());
+
+  // Create Task
+  auto testTaskOMP = std::make_shared<HorizontalSplitOMP_parallel>(taskDataPar);
+
+  // Create Perf attributes
+  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+  perfAttr->num_running = 10;
+  perfAttr->current_timer = [&] { return omp_get_wtime(); };
+
+  // Create and init perf results
+  auto perfResults = std::make_shared<ppc::core::PerfResults>();
+
+  // Create Perf analyzer
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskOMP);
+  perfAnalyzer->task_run(perfAttr, perfResults);
+  ppc::core::Perf::print_perf_statistic(perfResults);
+  for (int i = 0; i < n * m; i++) {
+    ASSERT_EQ(result[i], out_parallel[i]);
+  }
+}
