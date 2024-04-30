d4861da31e6cf85713154a683c185ed0fb032366
Valeria <lerasavotina@gmail.com>
2024-04-30 20:09:27 +0300

    fix clang-tidy

--- tasks/seq/savotina_v_grahams_alg/include/point.hpp
+++ tasks/seq/savotina_v_grahams_alg/include/point.hpp
@@ -16,7 +16,7 @@ struct SavotinaPoint {
   bool operator==(const SavotinaPoint& p2) const;
   bool operator()(SavotinaPoint& p0, SavotinaPoint& p1) const;
 
-  double angle(SavotinaPoint p);
+  double angle(const SavotinaPoint& p) const;
   double Distance(const SavotinaPoint& p) const;
   void Replace(SavotinaPoint& p2);
   int Compare(const SavotinaPoint& pivot, const SavotinaPoint& P) const;
