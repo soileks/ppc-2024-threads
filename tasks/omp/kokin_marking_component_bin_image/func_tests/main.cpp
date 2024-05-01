88bc1c967a3ba30abb023dfb67d0b1d28699ab9f
ThreeToads <ivan.kokin.465@gmail.com>
2024-05-02 02:27:15 +0300

    lab2: fix

--- tasks/omp/kokin_marking_component_bin_image/func_tests/main.cpp
+++ tasks/omp/kokin_marking_component_bin_image/func_tests/main.cpp
@@ -85,7 +85,7 @@ TEST(kokin_marking_component_bin_image, test_4) {
                              1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1};
   std::vector<uint8_t> out(ht * wh, 0);
   std::vector<uint8_t> cmpt = {1, 1, 0, 0, 2, 2, 2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 1, 1, 0, 0, 0,
-                                0, 0, 0, 0, 0, 0, 1, 0, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0, 0, 0};
+                               0, 0, 0, 0, 0, 0, 1, 0, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0, 0, 0};
 
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
   taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(size.data()));
@@ -106,7 +106,7 @@ TEST(kokin_marking_component_bin_image, test_4) {
 TEST(kokin_marking_component_bin_image, test_5_5_arr) {
   int ht = 5;
   int wh = 5;
-  std::vector<int> size = {ht, wh};;
+  std::vector<int> size = {ht, wh};
   std::vector<uint8_t> in = {0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1};
   std::vector<uint8_t> out(ht * wh, 0);
   std::vector<uint8_t> cmpt = {1, 1, 0, 0, 2, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 3, 0, 0, 0, 3, 3, 0, 0, 0};
