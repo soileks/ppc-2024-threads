700c7a7b57ddc0aae51b30a49d595989b608830a
luchshiy-shmel <shmelevivan02@yandex.ru>
2024-05-06 00:52:05 +0300

    tbb task

---
+++ tasks/tbb/shmelev_i_shell_sorting_with_Batcher/perf_tests/main.cpp
@@ -0,0 +1,76 @@
+// Copyright 2024 Shmelev Ivan
+#include <gtest/gtest.h>
+#include <oneapi/tbb.h>
+
+#include <vector>
+
+#include "core/perf/include/perf.hpp"
+#include "tbb/shmelev_i_shell_sorting_with_Batcher/include/ops_tbb.hpp"
+
+using namespace shmelev_tbb;
+
+TEST(shmelev_i_shell_sorting_with_Batcher, pipeline_run) {
+  const int count = 5000000;
+
+  // Create data
+  std::vector<int> in = ShmelevTaskTbb::generate_random_vector(count, 1, 1024);
+  std::vector<int> out(count, 0);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(&in));
+  taskDataSeq->inputs_count.emplace_back(in.size());
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(&out));
+  taskDataSeq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  auto testTaskTbb = std::make_shared<ShmelevTaskTbb>(taskDataSeq);
+
+  // Create Perf attributes
+  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+  perfAttr->num_running = 10;
+  const auto t0 = oneapi::tbb::tick_count::now();
+  perfAttr->current_timer = [&] { return (oneapi::tbb::tick_count::now() - t0).seconds(); };
+
+  // Create and init perf results
+  auto perfResults = std::make_shared<ppc::core::PerfResults>();
+
+  // Create Perf analyzer
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskTbb);
+  perfAnalyzer->pipeline_run(perfAttr, perfResults);
+  ppc::core::Perf::print_perf_statistic(perfResults);
+  ASSERT_TRUE(ShmelevTaskTbb::sorted(out));
+}
+
+TEST(shmelev_i_shell_sorting_with_Batcher, task_run) {
+  const int count = 5000000;
+
+  // Create data
+  std::vector<int> in = ShmelevTaskTbb::generate_random_vector(count, 1, 1024);
+  std::vector<int> out(count, 0);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(&in));
+  taskDataSeq->inputs_count.emplace_back(in.size());
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(&out));
+  taskDataSeq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  auto testTaskTbb = std::make_shared<ShmelevTaskTbb>(taskDataSeq);
+
+  // Create Perf attributes
+  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+  perfAttr->num_running = 10;
+  const auto t0 = oneapi::tbb::tick_count::now();
+  perfAttr->current_timer = [&] { return (oneapi::tbb::tick_count::now() - t0).seconds(); };
+
+  // Create and init perf results
+  auto perfResults = std::make_shared<ppc::core::PerfResults>();
+
+  // Create Perf analyzer
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskTbb);
+  perfAnalyzer->task_run(perfAttr, perfResults);
+  ppc::core::Perf::print_perf_statistic(perfResults);
+  ASSERT_TRUE(ShmelevTaskTbb::sorted(out));
+}
---
+++ tasks/tbb/shmelev_i_shell_sorting_with_Batcher/src/ops_tbb.cpp
@@ -0,0 +1,123 @@
+// Copyright 2024 Shmelev Ivan
+
+#include "tbb/shmelev_i_shell_sorting_with_Batcher/include/ops_tbb.hpp"
+
+#include <oneapi/tbb.h>
+
+#include <algorithm>
+
+bool shmelev_tbb::ShmelevTaskTbb::pre_processing() {
+  internal_order_test();
+  input_ = *reinterpret_cast<std::vector<int>*>(taskData->inputs[0]);
+  res = *reinterpret_cast<std::vector<int>*>(taskData->outputs[0]);
+  return true;
+}
+
+bool shmelev_tbb::ShmelevTaskTbb::validation() {
+  internal_order_test();
+  return taskData->inputs_count[0] == taskData->outputs_count[0];
+}
+
+bool shmelev_tbb::ShmelevTaskTbb::run() {
+  internal_order_test();
+  tbb::parallel_sort(input_.begin(), input_.end());
+  return true;
+}
+
+// bool shmelev_tbb::ShmelevTaskTbb::run() {
+//   internal_order_test();
+//   batcherMerge(0, input_.size() - 1);
+//   return true;
+// }
+
+bool shmelev_tbb::ShmelevTaskTbb::post_processing() {
+  internal_order_test();
+  reinterpret_cast<std::vector<int>*>(taskData->outputs[0])->operator=(input_);
+
+  return true;
+}
+
+std::vector<int> shmelev_tbb::ShmelevTaskTbb::generate_random_vector(int size, int min, int max) {
+  std::random_device rnd_device;
+  std::mt19937 mersenne_engine{rnd_device()};
+  std::uniform_int_distribution<int> dist{min, max};
+
+  auto gen = [&dist, &mersenne_engine]() { return dist(mersenne_engine); };
+
+  std::vector<int> vector(size);
+  generate(begin(vector), end(vector), gen);
+
+  return vector;
+}
+
+void shmelev_tbb::ShmelevTaskTbb::sortingShell() {
+  int n = input_.size();
+  for (int gap = n / 2; gap > 0; gap /= 2) {
+    for (int i = gap; i < n; i++) {
+      int temp = input_[i];
+      int j;
+      for (j = i; j >= gap && input_[j - gap] > temp; j -= gap) {
+        input_[j] = input_[j - gap];
+      }
+      input_[j] = temp;
+    }
+  }
+}
+
+void shmelev_tbb::ShmelevTaskTbb::batcherMerge(int l, int r) {
+  sortingShell();
+  if (r > l) {
+    int m = l + (r - l) / 2;
+    for (int i = l; i <= m; i += 2) {
+      if (i + 1 <= m) {
+        if (input_[i] > input_[i + 1]) {
+          std::swap(input_[i], input_[i + 1]);
+        }
+      }
+    }
+    batcherMerge(l, m);
+    batcherMerge(m + 1, r);
+    merge(l, m, r);
+  }
+}
+bool shmelev_tbb::ShmelevTaskTbb::sorted(std::vector<int> input) { return std::is_sorted(input.begin(), input.end()); }
+
+void shmelev_tbb::ShmelevTaskTbb::merge(int l, int m, int r) {
+  int n1 = m - l + 1;
+  int n2 = r - m;
+
+  std::vector<int> L(n1);
+  std::vector<int> R(n2);
+
+  for (int i = 0; i < n1; i++) {
+    L[i] = input_[l + i];
+  }
+  for (int j = 0; j < n2; j++) {
+    R[j] = input_[m + 1 + j];
+  }
+
+  int i = 0;
+  int j = 0;
+  int k = l;
+  while (i < n1 && j < n2) {
+    if (L[i] <= R[j]) {
+      input_[k] = L[i];
+      i++;
+    } else {
+      input_[k] = R[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (i < n1) {
+    input_[k] = L[i];
+    i++;
+    k++;
+  }
+  while (j < n2) {
+    input_[k] = R[j];
+    j++;
+    k++;
+  }
+}
