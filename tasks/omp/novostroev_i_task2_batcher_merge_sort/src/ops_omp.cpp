0cfa604f733edc5694fb17e6658b91f398f6bb45
Ivan Novostroev <novostroievv@mail.ru>
2024-05-07 21:10:56 +0300

    fix

--- tasks/omp/novostroev_i_task2_batcher_merge_sort/src/ops_omp.cpp
+++ tasks/omp/novostroev_i_task2_batcher_merge_sort/src/ops_omp.cpp
@@ -6,30 +6,55 @@
 
 using namespace std::chrono_literals;
 
-std::vector<int> mergeElements(std::vector<int>& vec1, std::vector<int>& vec2, size_t& idx1, size_t& idx2,
-                               size_t increment, int threads_count) {
-  std::vector<int> mergedVec(vec1.size() / 2 + vec2.size() / 2 + vec1.size() % 2 + vec2.size() % 2);
+std::vector<int> mergeElements(std::vector<int>& vec0, std::vector<int>& vec1, std::vector<int>& vec2, size_t& idx1, size_t& idx2,
+                               size_t increment, int modification, int threads_count) {
+  std::vector<int> mergedVec = vec0;
   size_t idx = 0;
 
-  while (idx1 < vec1.size() && idx2 < vec2.size()) {
-    if (vec1[idx1] <= vec2[idx2]) {
-      mergedVec[idx++] = vec1[idx1];
-      idx1 += increment;
-    } else {
-      mergedVec[idx++] = vec2[idx2];
-      idx2 += increment;
+  if ((modification == 0) || (modification == 1)) {
+    while ((idx1 < vec1.size()) && (idx2 < vec2.size())) {
+      if (vec1[idx1] <= vec2[idx2]) {
+        mergedVec[idx] = vec1[idx1];
+        idx1 += increment;
+      } else {
+        mergedVec[idx] = vec2[idx2];
+        idx2 += increment;
+      }
+      idx++;
     }
-  }
-
-  if (idx1 >= vec1.size()) {
+    if (idx1 >= vec1.size()) {
 #pragma omp parallel for num_threads(threads_count)
-    for (int i = idx2; i < static_cast<int>(vec2.size()); i += increment) {
-      mergedVec[idx++] = vec2[i];
+      for (int i = idx2; i < static_cast<int>(vec2.size()); i += increment) {
+        mergedVec[idx] = vec2[i];
+        idx++;
+      }
+    } else {
+#pragma omp parallel for num_threads(threads_count)
+      for (int i = idx1; i < static_cast<int>(vec1.size()); i += increment) {
+        mergedVec[idx] = vec1[i];
+        idx++;
+      }
     }
   } else {
+    while ((idx1 < vec1.size()) && (idx2 < vec2.size())) {
+      mergedVec[idx] = vec1[idx1];
+      mergedVec[idx + 1] = vec2[idx2];
+      idx += 2;
+      idx1++;
+      idx2++;
+    }
+    if ((idx2 >= vec2.size()) && (idx1 < vec1.size())) {
 #pragma omp parallel for num_threads(threads_count)
-    for (int i = idx1; i < static_cast<int>(vec1.size()); i += increment) {
-      mergedVec[idx++] = vec1[i];
+      for (int i = idx; i < static_cast<int>(mergedVec.size()); i += increment) {
+        mergedVec[i] = vec1[idx1];
+        idx1++;
+      }
+    }
+#pragma omp parallel for num_threads(threads_count)
+    for (int i = 0; i < static_cast<int>(mergedVec.size() - 1); i += increment) {
+      if (mergedVec[i] > mergedVec[i + 1]) {
+        std::swap(mergedVec[i], mergedVec[i + 1]);
+      }
     }
   }
 
@@ -38,24 +63,27 @@ std::vector<int> mergeElements(std::vector<int>& vec1, std::vector<int>& vec2, s
 
 std::vector<int> getOddElements(std::vector<int> vec1, std::vector<int> vec2, int threads_count = 4) {
   size_t idx1 = 1, idx2 = 1;
-  return mergeElements(vec1, vec2, idx1, idx2, 2, threads_count);
+  std::vector<int> mergedVec(vec1.size() / 2 + vec2.size() / 2);
+  return mergeElements(mergedVec, vec1, vec2, idx1, idx2, 2, 0, threads_count);
 }
 
 std::vector<int> getEvenElements(std::vector<int> vec1, std::vector<int> vec2, int threads_count = 4) {
   size_t idx1 = 0, idx2 = 0;
-  return mergeElements(vec1, vec2, idx1, idx2, 2, threads_count);
+  std::vector<int> mergedVec(vec1.size() / 2 + vec2.size() / 2 + vec1.size() % 2 + vec2.size() % 2);
+  return mergeElements(mergedVec, vec1, vec2, idx1, idx2, 2, 1, threads_count);
 }
 
 std::vector<int> mergeVectors(std::vector<int> vec1, std::vector<int> vec2, int threads_count = 4) {
   size_t idx1 = 0, idx2 = 0;
-  return mergeElements(vec1, vec2, idx1, idx2, 1, threads_count);
+  std::vector<int> mergedVec(vec1.size() + vec2.size());
+  return mergeElements(mergedVec, vec1, vec2, idx1, idx2, 1, 2, threads_count);
 }
 
-std::vector<int> sortBatcher(const std::vector<int>& leftHalf, const std::vector<int>& rightHalf) {
-  std::vector<int> mergedVec(leftHalf.size() + rightHalf.size());
-  merge(leftHalf.begin(), leftHalf.end(), rightHalf.begin(), rightHalf.end(), mergedVec.begin());
-  return mergedVec;
-}
+// std::vector<int> sortBatcher(const std::vector<int>& leftHalf, const std::vector<int>& rightHalf) {
+//   std::vector<int> mergedVec(leftHalf.size() + rightHalf.size());
+//   merge(leftHalf.begin(), leftHalf.end(), rightHalf.begin(), rightHalf.end(), mergedVec.begin());
+//   return mergedVec;
+// }
 
 std::vector<int> batcherSort(const std::vector<int>& vec1, const std::vector<int>& vec2) {
   std::vector<int> oddElements = getOddElements(vec1, vec2);
@@ -65,7 +93,7 @@ std::vector<int> batcherSort(const std::vector<int>& vec1, const std::vector<int
   if (!is_sorted(mergedVec.begin(), mergedVec.end())) {
     std::vector<int> leftHalf(mergedVec.begin(), mergedVec.begin() + mergedVec.size() / 2);
     std::vector<int> rightHalf(mergedVec.begin() + mergedVec.size() / 2, mergedVec.end());
-    return sortBatcher(leftHalf, rightHalf);
+    return batcherSort(leftHalf, rightHalf);
   }
 
   return mergedVec;
