c8a7abfaead36b7253aeef7b0267b2a34e716f8a
oleg212 <ipnnru@gmail.com>
2024-05-10 12:37:31 +0300

    seq version(test)

--- tasks/omp/pushkarev_i_dijkstra_shortest_path/src/dijkstra_shortest_path.cpp
+++ tasks/omp/pushkarev_i_dijkstra_shortest_path/src/dijkstra_shortest_path.cpp
@@ -31,58 +31,43 @@ bool DijkstraTaskOMP::validation() {
 
 bool DijkstraTaskOMP::run() {
   internal_order_test();
-
-  const size_t INF = std::numeric_limits<int>::max();
-
   size_t n = distances_.size();
   for (size_t i = 0; i < n; i++) {
-    distances_[i] = INF;
+    distances_[i] = std::numeric_limits<int>::max();
   }
   distances_[source] = 0;
   std::vector<bool> processed(n, false);
-  distances_[source] = 0;
 
-  // std::atomic<bool> end_flag(false);
-  // omp_set_num_threads(4);
-#pragma omp parallel
-  {
-#pragma omp single nowait
-    {
-      for (size_t i = 0; i < n; ++i) {
-#pragma omp task
-        {
-          size_t u = INF;
-          size_t min_dist = INF;
-
-// Find the node with shortest distance
-#pragma omp critical
-          {
-            for (size_t j = 0; j < n; ++j) {
-              if (!processed[j] && (distances_[j] < (int)min_dist)) {
-                u = j;
-                min_dist = distances_[j];
-              }
-            }
-          }
-
-#pragma omp critical
-          {
-            if (u != INF) {
-              // The Relax adjacent nodes
-              for (size_t v = 0; v < n; ++v) {
-                if (!processed[v] && (graph[u][v] != 0) && distances_[u] + graph[u][v] < distances_[v]) {
-                  distances_[v] = distances_[u] + graph[u][v];
-                }
-              }
-            }
-            processed[u] = true;
-          }
-        }
+  for (size_t count = 0; count < n; count++) {
+    size_t u = getMinDistanceVertex(processed);
+    processed[u] = true;
+    for (size_t v = 0; v < n; v++) {
+      if (!processed[v] && (graph[u][v] != 0) && (distances_[u] != std::numeric_limits<int>::max()) &&
+          distances_[u] + graph[u][v] < distances_[v]) {
+        relaxVertex(u, v);
       }
     }
   }
   return true;
 }
+
+size_t DijkstraTaskOMP::getMinDistanceVertex(const std::vector<bool>& processed) {
+  size_t min_dist = std::numeric_limits<size_t>::max();
+  size_t min_index = 0;
+
+  for (size_t v = 0; v < distances_.size(); v++) {
+    if (!processed[v] && (size_t)distances_[v] <= min_dist) {
+      min_dist = distances_[v];
+      min_index = v;
+    }
+  }
+  return min_index;
+}
+
+void DijkstraTaskOMP::relaxVertex(size_t u, size_t v) {
+  distances_[v] = std::min(distances_[v], distances_[u] + graph[u][v]);
+}
+
 
 bool DijkstraTaskOMP::post_processing() {
   internal_order_test();
