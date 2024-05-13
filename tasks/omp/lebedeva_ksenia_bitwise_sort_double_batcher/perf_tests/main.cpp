942e242921bcda8fe5d51fedbade7795bd468e0a
ks2k23 <lbdvks@mail.ru>
2024-05-14 02:48:56 +0300

    lab2_omp_6

--- tasks/omp/lebedeva_ksenia_bitwise_sort_double_batcher/perf_tests/main.cpp
+++ tasks/omp/lebedeva_ksenia_bitwise_sort_double_batcher/perf_tests/main.cpp
@@ -6,18 +6,18 @@
 
 TEST(lebedeva_ksenia_bitwise_sort_double_batcher, test_pipeline_run) {
   // Create data
-  std::vector<double> arr = LebedevaKOmp::randomVector(1000000, -1000000, 1000000);
-  std::vector<double> out(arr.size());
+  std::vector<double> array  = LebedevaKOmp::randomVector(1000000, -1000000, 1000000);
+  std::vector<double> out(array.size());
 
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> RSDBDataOmp = std::make_shared<ppc::core::TaskData>();
-  RSDBDataOmp->inputs.emplace_back(reinterpret_cast<uint8_t *>(arr.data()));
-  RSDBDataOmp->inputs_count.emplace_back(arr.size());
+  RSDBDataOmp->inputs.emplace_back(reinterpret_cast<uint8_t *>(array .data()));
+  RSDBDataOmp->inputs_count.emplace_back(array .size());
   RSDBDataOmp->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
   RSDBDataOmp->outputs_count.emplace_back(out.size());
 
-  std::vector<double> res(arr);
-  std::sort(res.begin(), res.end());
+  std::vector<double> result (array);
+  std::sort(result.begin(), result.end());
 
   // Create Task
   auto testRSDBompParallel = std::make_shared<LebedevaKOmp::RadixSortDoubleWithBatcherOmpParallel>(RSDBDataOmp);
@@ -34,25 +34,25 @@ TEST(lebedeva_ksenia_bitwise_sort_double_batcher, test_pipeline_run) {
   auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testRSDBompParallel);
   perfAnalyzer->pipeline_run(improvement, perfResults);
   ppc::core::Perf::print_perf_statistic(perfResults);
-  for (size_t i = 0; i < res.size(); ++i) {
-    ASSERT_EQ(res[i], out[i]);
+  for (size_t i = 0; i < result.size(); ++i) {
+    ASSERT_EQ(result[i], out[i]);
   }
 }
 
 TEST(lebedeva_ksenia_bitwise_sort_double_batcher, test_task_run) {
   // Create data
-  std::vector<double> arr = LebedevaK::randomVector(1000000, -1000000, 1000000);
-  std::vector<double> out(arr.size());
+  std::vector<double> array  = LebedevaK::randomVector(1000000, -1000000, 1000000);
+  std::vector<double> out(array .size());
 
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> RSDBDataOmp = std::make_shared<ppc::core::TaskData>();
-  RSDBDataOmp->inputs.emplace_back(reinterpret_cast<uint8_t *>(arr.data()));
-  RSDBDataOmp->inputs_count.emplace_back(arr.size());
+  RSDBDataOmp->inputs.emplace_back(reinterpret_cast<uint8_t *>(array .data()));
+  RSDBDataOmp->inputs_count.emplace_back(array .size());
   RSDBDataOmp->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
   RSDBDataOmp->outputs_count.emplace_back(out.size());
 
-  std::vector<double> res(arr);
-  std::sort(res.begin(), res.end());
+  std::vector<double> result(array);
+  std::sort(result.begin(), result.end());
 
   // Create Task
   auto testRSDBompParallel = std::make_shared<LebedevaKOmp::RadixSortDoubleWithBatcherOmpParallel>(RSDBDataOmp);
@@ -69,7 +69,7 @@ TEST(lebedeva_ksenia_bitwise_sort_double_batcher, test_task_run) {
   auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testRSDBompParallel);
   perfAnalyzer->task_run(improvement, perfResults);
   ppc::core::Perf::print_perf_statistic(perfResults);
-  for (size_t i = 0; i < res.size(); ++i) {
-    ASSERT_EQ(res[i], out[i]);
+  for (size_t i = 0; i < result.size(); ++i) {
+    ASSERT_EQ(result[i], out[i]);
   }
 }
