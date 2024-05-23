4d88f8c68bc325afdfcee59c7e4073b8f99cd361
luchshiy-shmel <shmelevivan02@yandex.ru>
2024-05-23 15:27:26 +0300

    4

--- tasks/stl/shmelev_i_shell_sorting_with_Batcher/src/ops_stl.cpp
+++ tasks/stl/shmelev_i_shell_sorting_with_Batcher/src/ops_stl.cpp
@@ -74,7 +74,8 @@ void shmelev_stl::ShmelevTaskSTL::batcherMerge(int l, int r) {
 bool shmelev_stl::ShmelevTaskSTL::sorted(std::vector<int> input) { return std::is_sorted(input.begin(), input.end()); }
 
 void shmelev_stl::ShmelevTaskSTL::merge(int l, int m, int r) {
-  int n1 = m - l + 1, n2 = r - m;
+  int n1 = m - l + 1;
+  int n2 = r - m;
 
   std::vector<int> L(n1);
   std::vector<int> R(n2);
@@ -86,7 +87,9 @@ void shmelev_stl::ShmelevTaskSTL::merge(int l, int m, int r) {
     R[j] = in[m + 1 + j];
   }
 
-  int i = 0, j = 0, k = l;
+  int i = 0;
+  int j = 0;
+  int k = l;
   while (i < n1 && j < n2) {
     if (L[i] <= R[j]) {
       in[k] = L[i];
