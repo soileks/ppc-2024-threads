93e016d5a73aefc334a207918bb61d470b77f720
Marissa696 <mrnsrtv@gmail.com>
2024-05-23 12:57:45 +0300

    fix codestyle2

--- tasks/stl/saratova_m_mult_matrix_fox/src/ops_stl.cpp
+++ tasks/stl/saratova_m_mult_matrix_fox/src/ops_stl.cpp
@@ -9,8 +9,8 @@
 bool saratova_stl::SaratovaTaskSTL::validation() {
   internal_order_test();
   return (taskData->inputs[0] != nullptr) && (taskData->inputs[1] != nullptr) && (taskData->outputs[0] != nullptr) &&
-          taskData->inputs_count[0] == taskData->inputs_count[1] &&
-          taskData->inputs_count[0] == taskData->outputs_count[0];
+        taskData->inputs_count[0] == taskData->inputs_count[1] &&
+        taskData->inputs_count[0] == taskData->outputs_count[0];
 }
 
 bool saratova_stl::SaratovaTaskSTL::pre_processing() {
@@ -25,35 +25,35 @@ bool saratova_stl::SaratovaTaskSTL::pre_processing() {
 
 void multiply_chunk(double* A, double* B, double* C, size_t n, size_t start, size_t end) {
   for (size_t i = start; i < end; i++) {
-      for (size_t j = 0; j < n; j++) {
-          double c = 0;
-          for (size_t k = 0; k < n; k++) {
-              c += A[i * n + k] * B[k * n + j];
-          }
-          C[i * n + j] = c;
+    for (size_t j = 0; j < n; j++) {
+      double c = 0;
+      for (size_t k = 0; k < n; k++) {
+        c += A[i * n + k] * B[k * n + j];
       }
+      C[i * n + j] = c;
+    }
   }
 }
 
 bool saratova_stl::SaratovaTaskSTL::run() {
   internal_order_test();
   try {
-      size_t num_threads = std::thread::hardware_concurrency();
-      size_t chunk_size = n / num_threads;
-      std::vector<std::thread> threads;
+    size_t num_threads = std::thread::hardware_concurrency();
+    size_t chunk_size = n / num_threads;
+    std::vector<std::thread> threads;
 
-      for (size_t t = 0; t < num_threads; ++t) {
-          size_t start = t * chunk_size;
-          size_t end = (t == num_threads - 1) ? n : start + chunk_size;
-          threads.emplace_back(multiply_chunk, A, B, C, n, start, end);
-      }
+    for (size_t t = 0; t < num_threads; ++t) {
+      size_t start = t * chunk_size;
+      size_t end = (t == num_threads - 1) ? n : start + chunk_size;
+      threads.emplace_back(multiply_chunk, A, B, C, n, start, end);
+    }
 
-      for (auto& thread : threads) {
-          thread.join();
-      }
+    for (auto& thread : threads) {
+      thread.join();
+    }
   } catch (const std::exception& e) {
-      std::cout << e.what() << std::endl;
-      return false;
+    std::cout << e.what() << std::endl;
+    return false;
   }
   return true;
 }
@@ -67,10 +67,10 @@ void saratova_stl::ScaledIdentityMatrix(double* matrix, int n, double k) {
   std::random_device dev;
   std::mt19937 gen(dev());
   for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-          matrix[i * n + (n - i - 1)] = 0;
-      }
-      matrix[i * n + (n - i - 1)] = k;
+    for (int j = 0; j < n; j++) {
+      matrix[i * n + (n - i - 1)] = 0;
+    }
+    matrix[i * n + (n - i - 1)] = k;
   }
 }
 
@@ -78,10 +78,10 @@ void saratova_stl::IdentityMatrix(double* matrix, int n, double k) {
   std::random_device dev;
   std::mt19937 gen(dev());
   for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-          matrix[i * n + j] = 0;
-      }
-      matrix[i * n + i] = k;
+    for (int j = 0; j < n; j++) {
+      matrix[i * n + j] = 0;
+    }
+    matrix[i * n + i] = k;
   }
 }
 
@@ -89,6 +89,6 @@ void saratova_stl::GenerateRandomValue(double* matrix, int sz) {
   std::random_device dev;
   std::mt19937 gen(dev());
   for (int i = 0; i < sz; i++) {
-      matrix[i] = gen() % 100;
+    matrix[i] = gen() % 100;
   }
 }
