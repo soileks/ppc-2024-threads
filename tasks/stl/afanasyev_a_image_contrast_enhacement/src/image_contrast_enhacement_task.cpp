ebe5cb464eaba94b2f6e28be9338753a48fdd781
Aleksey Afanasyev <aaastudy@mail.com>
2024-05-27 21:29:07 +0300

    fix lint (3)

--- tasks/stl/afanasyev_a_image_contrast_enhacement/src/image_contrast_enhacement_task.cpp
+++ tasks/stl/afanasyev_a_image_contrast_enhacement/src/image_contrast_enhacement_task.cpp
@@ -82,9 +82,14 @@ bool ImageContrastEnhacementTask::run() {
 
   for (std::size_t i = 0; i < threads.size(); i++) {
     std::size_t start_i = i * this->_input_pixels.size() / threads.size();
-    std::size_t end_i = (i == threads.size() - 1)
-        ? this->_input_pixels.size()
-        : (i + 1) * this->_input_pixels.size() / threads.size();
+
+    std::size_t end_i;
+    if (i == threads.size() - 1) {
+        end_i = this->_input_pixels.size();
+    } else {
+       (i + 1) * this->_input_pixels.size() / threads.size();
+    }
+
     threads[i] = std::thread(handle_pixel, start_i, end_i);
   }
 
