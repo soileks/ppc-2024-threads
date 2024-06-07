c5b96fb34f7906ee234df1f080a653e9a837711f
Mishaizlesa <misha11ru@gmail.com>
2024-06-08 00:29:20 +0300

    fix

--- tasks/stl/kozlov_m_simpson_integral/src/ops_stl.cpp
+++ tasks/stl/kozlov_m_simpson_integral/src/ops_stl.cpp
@@ -44,7 +44,7 @@ bool kozlov_stl::KozlovTaskSequential::run() {
       for (uint64_t j = 0; j <= m; j++) {
         if (j == 0 || j == m) {
           q = 1;
-        } else if (i % 2 == 0) {
+        } else if (j % 2 == 0) {
           q = 4;
         } else {
           q = 2;
