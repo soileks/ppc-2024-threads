d2208e7fe5c1f3734a00978ac33d1d0ebd1078bd
Nurlan Safarov <safarov_nm@mail.ru>
2024-06-07 22:14:42 +0300

    v1.1

--- tasks/stl/safarov_n_sparse_matmult_crs/src/sparse_matmult_crs_stl.cpp
+++ tasks/stl/safarov_n_sparse_matmult_crs/src/sparse_matmult_crs_stl.cpp
@@ -65,12 +65,9 @@ SparseMatrixCRS sparseMatrixTransposeCRS(const SparseMatrixCRS& object) {
 }
 
 bool SparseMatrixCRS::operator==(const SparseMatrixCRS& matrix) const {
-  if ((values == matrix.values) && (numberOfColumns == matrix.numberOfColumns) &&
-      (columnIndexes == matrix.columnIndexes) && (numberOfRows == matrix.numberOfRows) &&
-      (pointers == matrix.pointers)) {
-    return true;
-  }
-  return false;
+  return (values == matrix.values) && (numberOfColumns == matrix.numberOfColumns) &&
+         (columnIndexes == matrix.columnIndexes) && (numberOfRows == matrix.numberOfRows) &&
+         (pointers == matrix.pointers);
 }
 
 std::vector<std::vector<double>> fillTheMatrixWithZeros(int columns, int rows) {
@@ -193,7 +190,8 @@ bool SparseMatrixMultiplicationCRS_STL::pre_processing() {
 bool SparseMatrixMultiplicationCRS_STL::run() {
   internal_order_test();
 
-  std::vector<int> finalColumnIndexes, finalPointers;
+  std::vector<int> finalColumnIndexes;
+  std::vector<int> finalPointers;
   std::vector<double> finalValues;
   int resultRows = X->numberOfRows;
   std::vector<std::vector<int>> localColumnIndexes(X->numberOfRows);
