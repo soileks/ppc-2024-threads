31740805ae0883592fa09091025c313a0afc4b2c
AgentV0 <Vlad58009@gmail.com>
2024-06-08 17:35:41 +0300

    lab_omp_5

--- tasks/omp/ryabkov_v_int_merge_batcher/src/int_merge_batcher.cpp
+++ tasks/omp/ryabkov_v_int_merge_batcher/src/int_merge_batcher.cpp
@@ -1,6 +1,7 @@
 #include "omp/ryabkov_v_int_merge_batcher/include/int_merge_batcher.hpp"
 
 namespace ryabkov_batcher {
+
 void radix_sort(std::vector<int>& arr, int exp) {
   const std::size_t n = arr.size();
   std::vector<int> output(n);
@@ -9,34 +10,39 @@ void radix_sort(std::vector<int>& arr, int exp) {
 #pragma omp parallel
   {
     std::vector<int> local_count(10, 0);
+
 #pragma omp for
-    for (std::size_t i = 0; i < n; i++) {
+    for (std::size_t i = 0; i < n; ++i) {
       local_count[(arr[i] / exp) % 10]++;
     }
+
 #pragma omp critical
     {
-      for (int i = 0; i < 10; i++) {
+      for (int i = 0; i < 10; ++i) {
         count[i] += local_count[i];
       }
     }
   }
 
-  for (int i = 1; i < 10; i++) count[i] += count[i - 1];
-
-#pragma omp parallel for
-  for (int i = static_cast<int>(n) - 1; i >= 0; i--) {
-    output[count[(arr[i] / exp) % 10] - 1] = arr[i];
-#pragma omp atomic
-    count[(arr[i] / exp) % 10]--;
+  for (int i = 1; i < 10; i++) {
+    count[i] += count[i - 1];
   }
 
 #pragma omp parallel for
-  for (std::size_t i = 0; i < n; i++) arr[i] = output[i];
+  for (std::size_t i = n; i-- > 0;) {
+    int index = (arr[i] / exp) % 10;
+#pragma omp critical
+    output[--count[index]] = arr[i];
+  }
+
+#pragma omp parallel for
+  for (std::size_t i = 0; i < n; ++i) {
+    arr[i] = output[i];
+  }
 }
 
 void radix_sort(std::vector<int>& arr) {
   const int max_element = *std::max_element(arr.begin(), arr.end());
-
   for (int exp = 1; max_element / exp > 0; exp *= 10) {
     radix_sort(arr, exp);
   }
@@ -44,19 +50,21 @@ void radix_sort(std::vector<int>& arr) {
 
 std::vector<int> batch_merge(const std::vector<int>& a1, const std::vector<int>& a2) {
   std::vector<int> merged(a1.size() + a2.size());
-  std::size_t n1 = a1.size();
-  std::size_t n2 = a2.size();
 
-#pragma omp parallel
-  {
-    std::size_t local_i = 0, local_j = 0;
-#pragma omp for
-    for (std::size_t k = 0; k < merged.size(); ++k) {
-      if (local_i < n1 && (local_j >= n2 || a1[local_i] < a2[local_j])) {
-        merged[k] = a1[local_i++];
+#pragma omp parallel for
+  for (std::size_t k = 0; k < merged.size(); ++k) {
+    std::size_t i = 0, j = 0;
+    for (std::size_t m = 0; m < k; ++m) {
+      if (i < a1.size() && (j >= a2.size() || a1[i] < a2[j])) {
+        ++i;
       } else {
-        merged[k] = a2[local_j++];
+        ++j;
       }
+    }
+    if (i < a1.size() && (j >= a2.size() || a1[i] < a2[j])) {
+      merged[k] = a1[i++];
+    } else {
+      merged[k] = a2[j++];
     }
   }
   return merged;
@@ -67,7 +75,7 @@ std::vector<int> BatchSort(std::vector<int>& a1, std::vector<int>& a2) {
 
   for (size_t bit = 0; bit < sizeof(int) * 8; bit++) {
 #pragma omp parallel for
-    for (std::size_t i = 0; i < merged.size() / 2; i++) {
+    for (std::size_t i = 0; i < merged.size() / 2; ++i) {
       if (((i % 2 == 0) && ((merged[2 * i] >> bit) & 1) != 0) ||
           ((i % 2 != 0) && ((merged[2 * i + 1] >> bit) & 1) != 0)) {
         std::swap(merged[2 * i], merged[2 * i + 1]);
@@ -83,9 +91,8 @@ std::vector<int> BatchSort(std::vector<int>& a1, std::vector<int>& a2) {
 
   return merged;
 }
-}  // namespace ryabkov_batcher
 
-bool ryabkov_batcher::SeqBatcher::pre_processing() {
+bool SeqBatcher::pre_processing() {
   internal_order_test();
 
   if (!taskData) return false;
@@ -105,22 +112,24 @@ bool ryabkov_batcher::SeqBatcher::pre_processing() {
   return true;
 }
 
-bool ryabkov_batcher::SeqBatcher::validation() {
+bool SeqBatcher::validation() {
   internal_order_test();
 
   return taskData->inputs_count[0] == taskData->outputs_count[0];
 }
 
-bool ryabkov_batcher::SeqBatcher::run() {
+bool SeqBatcher::run() {
   internal_order_test();
 
-  result = ryabkov_batcher::BatchSort(a1, a2);
+  result = BatchSort(a1, a2);
   return true;
 }
 
-bool ryabkov_batcher::SeqBatcher::post_processing() {
+bool SeqBatcher::post_processing() {
   internal_order_test();
 
   std::copy(result.begin(), result.end(), reinterpret_cast<int*>(taskData->outputs[0]));
   return true;
 }
+
+}  // namespace ryabkov_batcher
