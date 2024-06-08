f41a32dcc7e41a3d5825b2737146e808574e7fbc
AgentV0 <Vlad58009@gmail.com>
2024-06-08 19:42:46 +0300

    lab_omp_15

--- tasks/omp/ryabkov_v_int_merge_batcher/src/int_merge_batcher.cpp
+++ tasks/omp/ryabkov_v_int_merge_batcher/src/int_merge_batcher.cpp
@@ -1,50 +1,43 @@
 #include "omp/ryabkov_v_int_merge_batcher/include/int_merge_batcher.hpp"
 
 namespace ryabkov_batcher {
-
 void radix_sort(std::vector<int>& arr, int exp) {
   const std::size_t n = arr.size();
   std::vector<int> output(n);
   std::vector<int> count(10, 0);
 
 #pragma omp parallel for
-  for (std::size_t i = 0; i < n; ++i) {
+  for (std::size_t i = 0; i < n; i++) count[(arr[i] / exp) % 10]++;
+
+  for (int i = 1; i < 10; i++) count[i] += count[i - 1];
+
+#pragma omp parallel for
+  for (int i = static_cast<int>(n) - 1; i >= 0; i--) {
+    output[count[(arr[i] / exp) % 10] - 1] = arr[i];
 #pragma omp atomic
-    count[(arr[i] / exp) % 10]++;
-  }
-
-  for (int i = 1; i < 10; i++) {
-    count[i] += count[i - 1];
-  }
-
-  for (int i = n - 1; i >= 0; --i) {
-    output[--count[(arr[i] / exp) % 10]] = arr[i];
+    count[(arr[i] / exp) % 10]--;
   }
 
 #pragma omp parallel for
-  for (std::size_t i = 0; i < n; ++i) {
-    arr[i] = output[i];
-  }
+  for (std::size_t i = 0; i < n; i++) arr[i] = output[i];
 }
 
 void radix_sort(std::vector<int>& arr) {
   const int max_element = *std::max_element(arr.begin(), arr.end());
-  for (int exp = 1; max_element / exp > 0; exp *= 10) {
-    radix_sort(arr, exp);
-  }
+
+  for (int exp = 1; max_element / exp > 0; exp *= 10) radix_sort(arr, exp);
 }
 
 std::vector<int> batch_merge(const std::vector<int>& a1, const std::vector<int>& a2) {
   std::vector<int> merged(a1.size() + a2.size());
+  std::size_t i = 0;
+  std::size_t j = 0;
 
-#pragma omp parallel for
   for (std::size_t k = 0; k < merged.size(); ++k) {
-    std::size_t i = k / 2;
-    std::size_t j = k - i;
     if (i < a1.size() && (j >= a2.size() || a1[i] < a2[j])) {
-      merged[k] = a1[i];
+      merged[k] = a1[i++];
     } else {
-      merged[k] = a2[j];
+      merged[k] = a2[j++];
     }
   }
   return merged;
@@ -53,11 +46,12 @@ std::vector<int> batch_merge(const std::vector<int>& a1, const std::vector<int>&
 std::vector<int> BatchSort(std::vector<int>& a1, std::vector<int>& a2) {
   std::vector<int> merged = batch_merge(a1, a2);
 
-  for (size_t bit = 0; bit < sizeof(int) * 8; bit++) {
 #pragma omp parallel for
-    for (std::size_t i = 0; i < merged.size() / 2; ++i) {
+  for (size_t bit = 0; bit < sizeof(int) * 8; bit++) {
+    for (std::size_t i = 0; i < merged.size() / 2; i++) {
       if (((i % 2 == 0) && ((merged[2 * i] >> bit) & 1) != 0) ||
           ((i % 2 != 0) && ((merged[2 * i + 1] >> bit) & 1) != 0)) {
+#pragma omp critical
         std::swap(merged[2 * i], merged[2 * i + 1]);
       }
     }
@@ -71,8 +65,9 @@ std::vector<int> BatchSort(std::vector<int>& a1, std::vector<int>& a2) {
 
   return merged;
 }
+}  // namespace ryabkov_batcher
 
-bool SeqBatcher::pre_processing() {
+bool ryabkov_batcher::SeqBatcher::pre_processing() {
   internal_order_test();
 
   if (!taskData) return false;
@@ -84,6 +79,7 @@ bool SeqBatcher::pre_processing() {
   a1.resize(inv.size() / 2);
   a2.resize(inv.size() / 2);
 
+#pragma omp parallel for
   for (std::size_t i = 0; i < inv.size() / 2; ++i) {
     a1[i] = inv[i];
     a2[i] = inv[inv.size() / 2 + i];
@@ -92,24 +88,22 @@ bool SeqBatcher::pre_processing() {
   return true;
 }
 
-bool SeqBatcher::validation() {
+bool ryabkov_batcher::SeqBatcher::validation() {
   internal_order_test();
 
   return taskData->inputs_count[0] == taskData->outputs_count[0];
 }
 
-bool SeqBatcher::run() {
+bool ryabkov_batcher::SeqBatcher::run() {
   internal_order_test();
 
-  result = BatchSort(a1, a2);
+  result = ryabkov_batcher::BatchSort(a1, a2);
   return true;
 }
 
-bool SeqBatcher::post_processing() {
+bool ryabkov_batcher::SeqBatcher::post_processing() {
   internal_order_test();
 
   std::copy(result.begin(), result.end(), reinterpret_cast<int*>(taskData->outputs[0]));
   return true;
 }
-
-}  // namespace ryabkov_batcher
