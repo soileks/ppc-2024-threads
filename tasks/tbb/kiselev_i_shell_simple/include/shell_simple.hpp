444476368109df6317e130b6511291b121e7d2d0
ISK7 <105171814+ISK7@users.noreply.github.com>
2024-06-08 14:12:03 +0300

    Added task

---
+++ tasks/tbb/kiselev_i_shell_simple/include/shell_simple.hpp
@@ -0,0 +1,32 @@
+// Copyright 2024 Kiselev Igor
+#ifndef SHELL_SIMPLE_HPP_INCLUDED
+#define SHELL_SIMPLE_HPP_INCLUDED
+#pragma once
+
+#include <vector>
+#include <tbb/tbb.h>
+
+#include "core/task/include/task.hpp"
+
+namespace Kiselev_tbb {
+
+class KiselevTaskTBB : public ppc::core::Task {
+ public:
+  explicit KiselevTaskTBB(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  std::vector<int> arr;
+  std::vector<int> input_;
+  int ThreadNum, DimSize;
+  bool IsSorted();
+  static int exp(int arg, int exp);
+  void FindThreadVariables();
+  void MergeBlocks(int Index1, int BlockSize1, int Index2, int BlockSize2);
+  void SeqSorter(int start, int end, std::vector<int> array);
+};
+}  // namespace Kiselev_tbb
+#endif  // SHELL_SIMPLE_HPP_INCLUDED
