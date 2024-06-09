bb8102220bf8631faf4f078920eb161cc37ccde8
AgentV0 <Vlad58009@gmail.com>
2024-06-09 16:11:09 +0300

    lab_omp_28

--- tasks/omp/ryabkov_v_int_merge_batcher/src/int_merge_batcher.cpp
+++ tasks/omp/ryabkov_v_int_merge_batcher/src/int_merge_batcher.cpp
@@ -10,7 +10,7 @@ void radix_sort(std::vector<int>& arr, int exp) {
   {
     std::vector<int> local_count(10, 0);
 #pragma omp for
-    for (std::size_t i = 0; i < n; i++) {
+    for (int i = 0; i < static_cast<int>(n); i++) {
       local_count[(arr[i] / exp) % 10]++;
     }
 
@@ -32,7 +32,7 @@ void radix_sort(std::vector<int>& arr, int exp) {
   }
 
 #pragma omp parallel for
-  for (std::size_t i = 0; i < n; i++) {
+  for (int i = 0; i < static_cast<int>(n); i++) {
     arr[i] = output[i];
   }
 }
@@ -47,11 +47,11 @@ void radix_sort(std::vector<int>& arr) {
 
 std::vector<int> batch_merge(const std::vector<int>& a1, const std::vector<int>& a2) {
   std::vector<int> merged(a1.size() + a2.size());
-  std::size_t i = 0;
-  std::size_t j = 0;
+  int i = 0;
+  int j = 0;
 
-  for (std::size_t k = 0; k < merged.size(); ++k) {
-    if (i < a1.size() && (j >= a2.size() || a1[i] < a2[j])) {
+  for (int k = 0; k < static_cast<int>(merged.size()); ++k) {
+    if (i < static_cast<int>(a1.size()) && (j >= static_cast<int>(a2.size()) || a1[i] < a2[j])) {
       merged[k] = a1[i++];
     } else {
       merged[k] = a2[j++];
@@ -63,9 +63,9 @@ std::vector<int> batch_merge(const std::vector<int>& a1, const std::vector<int>&
 std::vector<int> BatchSort(std::vector<int>& a1, std::vector<int>& a2) {
   std::vector<int> merged = batch_merge(a1, a2);
 
-  for (size_t bit = 0; bit < sizeof(int) * 8; bit++) {
+  for (int bit = 0; bit < sizeof(int) * 8; bit++) {
 #pragma omp parallel for
-    for (std::size_t i = 0; i < merged.size() / 2; i++) {
+    for (int i = 0; i < static_cast<int>(merged.size() / 2); i++) {
       if (((i % 2 == 0) && ((merged[2 * i] >> bit) & 1) != 0) ||
           ((i % 2 != 0) && ((merged[2 * i + 1] >> bit) & 1) != 0)) {
         std::swap(merged[2 * i], merged[2 * i + 1]);
@@ -75,7 +75,7 @@ std::vector<int> BatchSort(std::vector<int>& a1, std::vector<int>& a2) {
 
   radix_sort(merged);
 
-  std::size_t n = merged.size() / 2;
+  int n = static_cast<int>(merged.size() / 2);
   a1.assign(merged.begin(), merged.begin() + n);
   a2.assign(merged.begin() + n, merged.end());
 
@@ -96,7 +96,7 @@ bool ryabkov_batcher::SeqBatcher::pre_processing() {
   a2.resize(inv.size() / 2);
 
 #pragma omp parallel for
-  for (std::size_t i = 0; i < inv.size() / 2; ++i) {
+  for (int i = 0; i < static_cast<int>(inv.size() / 2); ++i) {
     a1[i] = inv[i];
     a2[i] = inv[inv.size() / 2 + i];
   }
