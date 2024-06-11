4e1f80ada7207fb3281a2c7705ca7aebb2342914
Филатов Максим <999099917@mail.ru>
2024-06-12 02:38:09 +0300

    update tests

--- tasks/tbb/filatov_m_linear_image_filtering_tbb/perf_tests/main.cpp
+++ tasks/tbb/filatov_m_linear_image_filtering_tbb/perf_tests/main.cpp
@@ -6,90 +6,89 @@
 #include "core/perf/include/perf.hpp"
 #include "tbb/filatov_m_linear_image_filtering_tbb/include/ops_tbb.hpp"
 
-TEST(filatov_m_linear_image_filtering, test_pipeline_run_with_blue_image) {
-  uint32_t width = 1024;
-  uint32_t height = 1024;
+class filatov_m_linear_image_filtering : public ::testing::Test {
+ protected:
+  std::shared_ptr<ppc::core::TaskData> taskData;
+  std::vector<uint8_t> inputData;
+  std::vector<uint8_t> outputData;
+  std::vector<uint8_t> expectedData;
+  uint64_t width;
+  uint64_t height;
+  std::shared_ptr<GaussFilterHorizontal> gaussFilterHorizontal;
 
-  // Create data
-  std::vector<uint8_t> in(width * height * 3, 0);
-  for (size_t i = 2; i < in.size(); i += 3) {
-    in[i] = 255;  // Blue channel
+  // Передайте параметр по константной ссылке для копирования
+  void SetExpectedData(const std::vector<uint8_t>& data) { expectedData = data; }
+
+  void SetUpWH(int width_, int height_) {
+    width = width_;
+    height = height_;
   }
-  std::vector<uint8_t> out(width * height * 3, 0);
-  std::vector<uint8_t> expected = in;
 
-  // Create TaskData
-  std::shared_ptr<ppc::core::TaskData> taskImageData = std::make_shared<ppc::core::TaskData>();
-  taskImageData->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
-  taskImageData->inputs_count.emplace_back(width);
-  taskImageData->inputs_count.emplace_back(height);
-  taskImageData->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
-  taskImageData->outputs_count.emplace_back(width);
-  taskImageData->outputs_count.emplace_back(height);
+  void CreateGaussFilterHorizontal() { gaussFilterHorizontal = std::make_shared<GaussFilterHorizontal>(taskData); }
 
-  // Create Task
-  auto gaussFilterHorizontal = std::make_shared<GaussFilterHorizontal>(taskImageData);
+  void CommonSetUp() {
+    inputData.resize(width * height * 3, 0);
+    outputData.resize(width * height * 3, 0);
+    expectedData.resize(width * height * 3, 0);
+    taskData = std::make_shared<ppc::core::TaskData>();
+  }
 
-  // Create Perf attributes
-  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
-  perfAttr->num_running = 10;
-  const auto t0 = std::chrono::high_resolution_clock::now();
-  perfAttr->current_timer = [&] {
-    auto current_time_point = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
-    return static_cast<double>(duration) * 1e-9;
-  };
+  void InitializeTaskData() {
+    taskData->inputs.push_back(reinterpret_cast<uint8_t*>(inputData.data()));
+    taskData->inputs_count.push_back(width);
+    taskData->inputs_count.push_back(height);
+    taskData->outputs.push_back(reinterpret_cast<uint8_t*>(outputData.data()));
+    taskData->outputs_count.push_back(width);
+    taskData->outputs_count.push_back(height);
+  }
 
-  // Create and init perf results
-  auto perfResults = std::make_shared<ppc::core::PerfResults>();
+  void FillChannel(size_t channel, uint8_t value) {
+    for (size_t i = channel; i < inputData.size(); i += 3) {
+      inputData[i] = value;
+    }
+  }
 
-  // Create Perf analyzer
-  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(gaussFilterHorizontal);
-  perfAnalyzer->pipeline_run(perfAttr, perfResults);
-  ppc::core::Perf::print_perf_statistic(perfResults);
-  ASSERT_EQ(expected, out);
+  void ValidateOutputData() { ASSERT_EQ(expectedData, outputData); }
+
+  static std::shared_ptr<ppc::core::PerfAttr> SetUpPerfAttributes() {
+    auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+    perfAttr->num_running = 10;
+    return perfAttr;
+  }
+
+  void static RunPerfAnalysis(const std::shared_ptr<GaussFilterHorizontal>& filter) {
+    auto perfAttr = SetUpPerfAttributes();
+    const auto t0 = std::chrono::high_resolution_clock::now();
+    perfAttr->current_timer = [&] {
+      auto current_time_point = std::chrono::high_resolution_clock::now();
+      auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
+      return static_cast<double>(duration) * 1e-9;
+    };
+    auto perfResults = std::make_shared<ppc::core::PerfResults>();
+    auto perfAnalyzer = std::make_shared<ppc::core::Perf>(filter);
+    perfAnalyzer->pipeline_run(perfAttr, perfResults);
+    ppc::core::Perf::print_perf_statistic(perfResults);
+  }
+};
+
+TEST_F(filatov_m_linear_image_filtering, test_pipeline_run_with_red_image) {
+  SetUpWH(512, 512);
+  CommonSetUp();
+  FillChannel(0, 111);
+  CreateGaussFilterHorizontal();
+  SetExpectedData(inputData);
+  InitializeTaskData();
+  RunPerfAnalysis(gaussFilterHorizontal);
+  ValidateOutputData();
 }
 
-TEST(filatov_m_linear_image_filtering, test_task_run_with_green_image) {
-  uint32_t width = 1024;
-  uint32_t height = 1024;
-
-  // Create data
-  std::vector<uint8_t> in(width * height * 3, 0);
-  for (size_t i = 1; i < in.size(); i += 3) {
-    in[i] = 255;  // Green channel
-  }
-  std::vector<uint8_t> out(width * height * 3, 0);
-  std::vector<uint8_t> expected = in;
-
-  // Create TaskData
-  std::shared_ptr<ppc::core::TaskData> taskImageData = std::make_shared<ppc::core::TaskData>();
-  taskImageData->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
-  taskImageData->inputs_count.emplace_back(width);
-  taskImageData->inputs_count.emplace_back(height);
-  taskImageData->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
-  taskImageData->outputs_count.emplace_back(width);
-  taskImageData->outputs_count.emplace_back(height);
-
-  // Create Task
-  auto gaussFilterHorizontal = std::make_shared<GaussFilterHorizontal>(taskImageData);
-
-  // Create Perf attributes
-  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
-  perfAttr->num_running = 10;
-  const auto t0 = std::chrono::high_resolution_clock::now();
-  perfAttr->current_timer = [&] {
-    auto current_time_point = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
-    return static_cast<double>(duration) * 1e-9;
-  };
-
-  // Create and init perf results
-  auto perfResults = std::make_shared<ppc::core::PerfResults>();
-
-  // Create Perf analyzer
-  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(gaussFilterHorizontal);
-  perfAnalyzer->task_run(perfAttr, perfResults);
-  ppc::core::Perf::print_perf_statistic(perfResults);
-  ASSERT_EQ(expected, out);
+TEST_F(filatov_m_linear_image_filtering, test_task_run_with_blue_image) {
+  SetUpWH(512, 512);
+  CommonSetUp();
+  FillChannel(1, 255);
+  CreateGaussFilterHorizontal();
+  SetExpectedData(inputData);
+  InitializeTaskData();
+  RunPerfAnalysis(gaussFilterHorizontal);
+  ValidateOutputData();
 }
