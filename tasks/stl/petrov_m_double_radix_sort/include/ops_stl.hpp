4f2245ca23498c4f11823d8e79856beb50265984
Maksim <redbulprofi@gmail.com>
2024-06-12 19:19:54 +0300

    1 commit

---
+++ tasks/stl/petrov_m_double_radix_sort/include/ops_stl.hpp
@@ -0,0 +1,35 @@
+
+#pragma once
+
+#include <algorithm>
+#include <future>
+#include <iterator>
+#include <random>
+#include <thread>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+using namespace std::chrono_literals;
+namespace petrov_stl {
+
+class PetrovRadixSortDoubleSTL : public ppc::core::Task {
+ public:
+  explicit PetrovRadixSortDoubleSTL(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  int data_size;
+  std::vector<double> sort;
+  static void PetrovCountSort(double* in, double* out, int len, int exp);
+  static bool PetrovCountSortSigns(const double* in, double* out, int len);
+  std::vector<double> PetrovRadixSort(const std::vector<double>& data1);
+  static std::vector<std::vector<double>> PetrovSplitVector(const std::vector<double>& data, int numParts);
+  std::vector<double> PetrovMerge(const std::vector<double>& arr1, const std::vector<double>& arr2);
+  std::vector<double> PetrovRadixSortStl(const std::vector<double>& data, int numParts);
+  std::vector<double> PetrovBinaryMergeTree(std::vector<std::vector<double>>& sortedVectors);
+};
+}  // namespace petrov_stl
