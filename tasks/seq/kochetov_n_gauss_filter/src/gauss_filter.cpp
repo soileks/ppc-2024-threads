ef76576dad6a17ec20deb91131d66b841b12b757
esthete014 <nikkochetov@outlook.com>
2024-06-12 20:21:52 +0300

    fix

--- tasks/seq/kochetov_n_gauss_filter/src/gauss_filter.cpp
+++ tasks/seq/kochetov_n_gauss_filter/src/gauss_filter.cpp
@@ -60,7 +60,7 @@ bool GaussFilter::validation() {
   internal_order_test();
 
   return !taskData->inputs.empty() && !taskData->outputs.empty() && !taskData->inputs_count.empty() &&
-          !taskData->outputs_count.empty() && taskData->inputs[0] != nullptr && taskData->outputs[0] != nullptr &&
-          taskData->outputs_count[1] == taskData->inputs_count[1] && taskData->outputs_count[0] >= 3 &&
-          taskData->outputs_count[1] >= 3;
+         !taskData->outputs_count.empty() && taskData->inputs[0] != nullptr && taskData->outputs[0] != nullptr &&
+         taskData->outputs_count[1] == taskData->inputs_count[1] && taskData->outputs_count[0] >= 3 &&
+         taskData->outputs_count[1] >= 3;
 }
