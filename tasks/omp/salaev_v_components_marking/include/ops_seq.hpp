c945a5462f4f6078130c0ae261f861d557e79dd4
hadnle-less <salaev-08@mail.ru>
2024-06-13 19:26:53 +0300

    2 lab

---
+++ tasks/omp/salaev_v_components_marking/include/ops_seq.hpp
@@ -0,0 +1,46 @@
+// Copyright 2024 Salaev Vladislav
+#pragma once
+
+#include <algorithm>
+#include <random>
+#include <set>
+#include <thread>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace SalaevOMP {
+
+class ImageMarkingSeq : public ppc::core::Task {
+ public:
+  explicit ImageMarkingSeq(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  std::vector<std::vector<uint8_t>> source = {};
+  std::vector<std::vector<uint32_t>> destination = {};
+  uint32_t height{}, width{};
+  void resolve_labels(std::vector<int>& labels);
+};
+
+class ImageMarkingOmp : public ppc::core::Task {
+ public:
+  explicit ImageMarkingOmp(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  std::vector<std::vector<uint8_t>> source = {};
+  std::vector<std::vector<uint32_t>> destination = {};
+  uint32_t height{}, width{};
+  void resolve_labels(std::vector<int>& labels);
+};
+
+}  // namespace SalaevOMP
