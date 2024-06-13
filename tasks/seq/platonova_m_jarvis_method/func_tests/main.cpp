30e392c994374722f193a09dda71a8417cab05d6
PlatonovaMariya <mari.platonova.2003@inbox.ru>
2024-06-13 13:29:14 +0300

    fix

--- tasks/seq/platonova_m_jarvis_method/func_tests/main.cpp
+++ tasks/seq/platonova_m_jarvis_method/func_tests/main.cpp
@@ -66,9 +66,9 @@ TEST(platonova_m_jarvis_method, diagonal_points) {
   ASSERT_EQ(resHull[1], hull[1]);
 }
 
-TEST(platonova_m_jarvis_method, Duplicate_Points) {
-  std::vector<Point> points = {{0, 0}, {1, 1}, {2, 2}, {0, 0}, {1, 1}, {2, 2}};
-  std::vector<Point> hull = {{0, 0}, {1, 1}, {2, 2}};
+TEST(platonova_m_jarvis_method, diagonal_points2) {
+  std::vector<Point> points = {{0, 0}, {1, 1}, {2, 2}, {3, 3}, {4, 4}, {5, 5}, {6, 6}, {7, 7}};
+  std::vector<Point> hull = points;
   std::vector<Point> resHull(hull.size());
 
   // Create TaskData
@@ -84,7 +84,6 @@ TEST(platonova_m_jarvis_method, Duplicate_Points) {
   testTaskSequential.pre_processing();
   testTaskSequential.run();
   testTaskSequential.post_processing();
-
   ASSERT_EQ(resHull[0], hull[0]);
   ASSERT_EQ(resHull[1], hull[1]);
 }
