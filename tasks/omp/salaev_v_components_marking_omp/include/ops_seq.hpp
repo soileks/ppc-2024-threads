41b1aa29767322c3d5f543417a46c487829103e5
hadnle-less <salaev-08@mail.ru>
2024-06-13 20:03:04 +0300

    clang fix

--- tasks/omp/salaev_v_components_marking_omp/include/ops_seq.hpp
+++ tasks/omp/salaev_v_components_marking_omp/include/ops_seq.hpp
@@ -25,7 +25,7 @@ class ImageMarkingSeq : public ppc::core::Task {
   std::vector<std::vector<uint8_t>> source = {};
   std::vector<std::vector<uint32_t>> destination = {};
   uint32_t height{}, width{};
-  void resolve_labels(std::vector<int>& labels);
+  static void resolve_labels(std::vector<int>& labels);
 };
 
 class ImageMarkingOmp : public ppc::core::Task {
