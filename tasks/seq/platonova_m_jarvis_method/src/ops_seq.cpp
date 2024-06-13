eea9e7ce13bf4b9d9fa37a24def9474748ff5183
PlatonovaMariya <mari.platonova.2003@inbox.ru>
2024-06-13 12:52:41 +0300

    fix

--- tasks/seq/platonova_m_jarvis_method/src/ops_seq.cpp
+++ tasks/seq/platonova_m_jarvis_method/src/ops_seq.cpp
@@ -10,7 +10,7 @@
 
 using namespace std::chrono_literals;
 
-int orientation(Point p, Point q, Point r) {
+int checkOrient(Point p, Point q, Point r) {
   int val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
   if (val == 0) return 0;
   return (val > 0) ? -1 : 1;
@@ -32,7 +32,7 @@ std::vector<Point> Jarvis(const std::vector<Point>& points) {
     hull.push_back(points[p]);
     q = (p + 1) % n;
     for (int i = 0; i < n; i++) {
-      if (orientation(points[p], points[i], points[q]) == 1) q = i;
+      if (checkOrient(points[p], points[i], points[q]) == 1) q = i;
     }
     p = q;
   } while (p != l);
